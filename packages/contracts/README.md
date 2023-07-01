2cac22f3bc48fa0ee3811452aa4322bd6a50af0d
Ian Lucas <ian.lucas@gmail.com>
2023-07-01 20:26:45 +0200

    Add Deployment docs to README

--- packages/contracts/README.md
+++ packages/contracts/README.md
@@ -12,3 +12,21 @@ forge build
 ```bash
 forge test
 ```
+
+## Deploy and Verify
+```bash
+source .env
+
+forge create --rpc-url $RPC_URL \
+    --constructor-args 1000 \
+    --private-key  $PRIVATE_KEY \
+    --etherscan-api-key $BLOCKCHAIN_EXPLORER_API_KEY \
+    --verify \
+    src/CredbullToken.sol:CredbullToken
+ ```
+
+## Static code analysis
+See: https://book.getfoundry.sh/config/static-analyzers
+```bash
+myth analyze src/CredbullToken.sol --solc-json mythril.config.json --execution-timeout 60
+```
--- packages/contracts/src/CredbullToken.sol
+++ packages/contracts/src/CredbullToken.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-import "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
-import "openzeppelin-contracts/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import "openzeppelin-contracts/contracts/access/Ownable.sol";
+import "lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+import "lib/openzeppelin-contracts/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "lib/openzeppelin-contracts/contracts/access/Ownable.sol";
 
 contract CredbullToken is ERC20, ERC20Burnable, Ownable {
 
