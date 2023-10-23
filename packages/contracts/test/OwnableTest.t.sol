45afa17e63d0a7fd6e68c565cd8fe09c12696ba4
Ian Lucas <ian.lucas@gmail.com>
2023-10-23 14:27:42 +0200

    Adding Course-grained Ownership tests

--- packages/contracts/test/OwnableTest.t.sol
+++ packages/contracts/test/OwnableTest.t.sol
@@ -8,12 +8,18 @@ import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 // Vaults exchange Assets for Shares in the Vault
 // see: https://eips.ethereum.org/EIPS/eip-4626
 contract MyContract is Ownable {
-    function normalThing() public {
-        // anyone can call this normalThing()
+    uint256 public number;
+
+    function setNumber(uint256 newNumber) public onlyOwner {
+        number = newNumber;
     }
 
-    function specialThing() public onlyOwner {
-        // only the owner can call specialThing()!
+    function increment() public onlyOwner {
+        number++;
+    }
+
+    function doNothing() public {
+        // anyone can call this normalThing()
     }
 }
 
@@ -35,4 +41,30 @@ contract CredbullMultiSigVaultTest is Test {
 
         assertEq(myContract.owner(), msg.sender);
     }
+
+    function testOwnerCanSetNumber() public {
+        uint256 number = 10;
+
+        console.log("Token Owner", myContract.owner());
+        console.log("Msg Sender", msg.sender);
+
+        assertEq(myContract.owner(), msg.sender);
+
+        vm.prank(msg.sender); // owner only call.  owner is  msg.sender.
+        myContract.setNumber(number);
+
+        assertEq(number, myContract.number());
+    }
+
+    // test some other user can't do specialThing
+    function testNonOwnerThrowsError() public {
+        address nonOwnerAddress = address(0x1);
+
+        assertEq(myContract.owner(), msg.sender);
+
+        vm.prank(nonOwnerAddress); // owner only call.  owner is  msg.sender.
+        vm.expectRevert();
+        myContract.setNumber(0);
+    }
+
 }
