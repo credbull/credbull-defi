fb85a4dea8a701a5ba06afd7adcd453e1bce73b1
Pedro Viegas <pedroasv@gmail.com>
2023-11-05 08:20:38 -0300

    feat(#3 share multiplier): adding dynamic multipliers per address so configured users can receive more shares per assets. another possible approach would be having different shares managed by the same vault.

--- spikes/share-muliplier/test/SimpleShareMultiplierVault.t.sol
+++ spikes/share-muliplier/test/SimpleShareMultiplierVault.t.sol
@@ -2,20 +2,58 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
+import "forge-std/console.sol";
 import "../contracts/SimpleToken.sol";
 import "../contracts/SimpleShareMultiplierVault.sol";
 
 contract SimpleShareMultiplierVaultTest is Test {
-  SimpleToken public token;
-  SimpleShareMultiplierVault public vault;
+    SimpleToken public token;
+    SimpleShareMultiplierVault public vault;
 
-  function setUp() public {
-    token = new SimpleToken(1000);
-    vault = new SimpleShareMultiplierVault(token, "SimpleShareMultiplierVault", "xSIM");
-  }
+    function setUp() public {
+        token = new SimpleToken(2000);
+        vault = new SimpleShareMultiplierVault(token, "SimpleShareMultiplierVault", "xSIM");
+    }
 
-  function test() public {
-    token.mint(address(vault), 1000);
-    require(token.balanceOf(address(vault)) == 1000);
-  }
+    function testDepositWithMultiplier() public {
+        token.mint(msg.sender, 1000);
+        token.approve(address(vault), 1000);
+
+        vault.addMultiplier(msg.sender, 2);
+        vault.deposit(1000, msg.sender);
+
+        require(token.balanceOf(address(vault)) == 1000);
+        require(vault.balanceOf(address(msg.sender)) == 2000);
+    }
+
+    function testMintWithMultiplier() public {
+        token.mint(msg.sender, 1000);
+        token.approve(address(vault), 1000);
+
+        vault.addMultiplier(msg.sender, 2);
+        vault.mint(2000, msg.sender);
+
+        require(token.balanceOf(address(vault)) == 1000);
+        require(vault.balanceOf(address(msg.sender)) == 2000);
+    }
+
+    function testDepositWithoutMultiplier() public {
+        token.mint(msg.sender, 1000);
+        token.approve(address(vault), 1000);
+
+        vault.deposit(1000, msg.sender);
+
+        require(token.balanceOf(address(vault)) == 1000);
+        require(vault.balanceOf(address(msg.sender)) == 1000);
+    }
+
+    function testMintWithoutMultiplier() public {
+        token.mint(msg.sender, 1000);
+        token.approve(address(vault), 1000);
+
+        vault.mint(1000, msg.sender);
+
+        require(token.balanceOf(address(vault)) == 1000);
+        require(vault.balanceOf(address(msg.sender)) == 1000);
+    }
 }
