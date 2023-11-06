112e8f0b83095de6004c147dd806af4b77af3a50
Pedro Viegas <pedroasv@gmail.com>
2023-11-06 07:53:56 -0300

    feat(#2 vault timelock): using a vesting wallet to timelock a vault

--- spikes/timelock/contracts/SimpleTokenVesting.sol
+++ spikes/timelock/contracts/SimpleTokenVesting.sol
@@ -3,9 +3,49 @@ pragma solidity >=0.8.0 <0.9.0;
 
 import "@openzeppelin/contracts/interfaces/IERC20.sol";
 import "@openzeppelin/contracts/finance/VestingWallet.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
 
-contract SimpleTokenVesting is VestingWallet {
-  constructor(address beneficiary, uint64 startTimestamp, uint64 durationSeconds)
-  VestingWallet(beneficiary, startTimestamp, durationSeconds)
-  {}
+contract SimpleTokenVesting is ERC4626 {
+
+    error SharesInVestingPeriod();
+
+    VestingWallet public vestingWallet;
+    IERC20 private immutable _asset;
+
+    constructor(IERC20 asset, string memory name, string memory symbol, uint64 startTimestamp, uint64 durationSeconds)
+    ERC4626(asset)
+    ERC20(name, symbol)
+    {
+        _asset = asset;
+        vestingWallet = new VestingWallet(address(this), startTimestamp, durationSeconds);
+    }
+
+    function totalAssets() public view virtual override returns (uint256) {
+        return _asset.balanceOf(address(vestingWallet));
+    }
+
+    function _deposit(address caller, address receiver, uint256 assets, uint256 shares) internal virtual override {
+        SafeERC20.safeTransferFrom(_asset, caller, address(vestingWallet), assets);
+        _mint(receiver, shares);
+
+        emit Deposit(caller, receiver, assets, shares);
+    }
+
+    function _withdraw(address caller, address receiver, address owner, uint256 assets, uint256 shares) internal virtual override {
+        if (assets > vestingWallet.releasable(address(_asset))) {
+            revert SharesInVestingPeriod();
+        }
+
+        vestingWallet.release(address(_asset));
+
+        if (caller != owner) {
+            _spendAllowance(owner, caller, shares);
+        }
+
+        _burn(owner, shares);
+        SafeERC20.safeTransfer(_asset, receiver, assets);
+
+        emit Withdraw(caller, receiver, owner, assets, shares);
+    }
 }
--- spikes/timelock/test/SimpleTokenVesting.t.sol
+++ spikes/timelock/test/SimpleTokenVesting.t.sol
@@ -2,28 +2,48 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
+import "forge-std/console.sol";
 import "../contracts/SimpleTokenVesting.sol";
 import "../contracts/SimpleToken.sol";
 
 contract SimpleTokenVestingTest is Test {
   SimpleToken public token;
-  SimpleTokenVesting public lock;
+  SimpleTokenVesting public vault;
 
   function setUp() public {
     token = new SimpleToken(1000);
-    lock = new SimpleTokenVesting(address(this), uint64(block.timestamp), 1);
-  }
-
-  function testShouldNotReleaseVesting() public {
-    token.mint(address(lock), 1000);
-    lock.release(address(token));
-    require(lock.released(address(token)) == 0);
+    vault = new SimpleTokenVesting(token, "SimpleTokenVesting", "xSIM", uint64(block.timestamp), 1);
   }
 
   function testShouldReleaseVesting() public {
-    token.mint(address(lock), 1000);
+    address john = makeAddr("John");
+    token.mint(john, 1000);
+
+    vm.startPrank(john);
+    token.approve(address(vault), 2000);
+    vault.deposit(1000, john);
+
+    require(vault.totalAssets() == 1000);
+    require(token.balanceOf(address(vault.vestingWallet())) == 1000);
+    require(vault.balanceOf(address(john)) == 1000);
+
     vm.warp(block.timestamp + 2);
-    lock.release(address(token));
-    require(lock.released(address(token)) == 1000);
+    vault.withdraw(1000, john, john);
+
+    require(token.balanceOf(address(vault)) == 0);
+    require(token.balanceOf(address(john)) == 1000);
+    require(vault.balanceOf(address(john)) == 0);
+  }
+
+  function testShouldNotReleaseVesting() public {
+    address john = makeAddr("John");
+    token.mint(john, 1000);
+
+    vm.startPrank(john);
+    token.approve(address(vault), 2000);
+    vault.deposit(1000, john);
+
+    vm.expectRevert(SimpleTokenVesting.SharesInVestingPeriod.selector);
+    vault.withdraw(1000, john, john);
   }
 }
