056a7f5c2e4ba5e3b5f9408399672c6246fa42c0
Pedro Viegas <pedroasv@gmail.com>
2023-11-11 16:11:43 -0300

    feat(#3) timelock using a modifier applied to a vault (foundry)

---
+++ packages/gnosis-timelock/src/test/ButtonPushModule.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: LGPL-3.0-only
+pragma solidity ^0.8.9;
+
+import "@gnosis.pm/zodiac/contracts/core/Module.sol";
+import "hardhat/console.sol";
+
+contract ButtonPushModule is Module {
+    address public button;
+
+    constructor(address _owner, address _button) {
+        bytes memory initializeParams = abi.encode(_owner, _button);
+        setUp(initializeParams);
+    }
+
+    function setUp(bytes memory initializeParams) public override initializer {
+        __Ownable_init(_msgSender());
+
+        (address _owner, address _button) = abi.decode(initializeParams, (address, address));
+
+        button = _button;
+        setAvatar(_owner);
+        setTarget(_owner);
+        transferOwnership(_owner);
+    }
+
+    function pushButton() external {
+        exec(button, 0, abi.encodePacked(bytes4(keccak256("pushButton()"))), Enum.Operation.Call);
+    }
+}
