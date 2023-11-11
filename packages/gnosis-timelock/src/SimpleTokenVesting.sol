c1309903d1aee0cfd25be5b2a32d1cfba36a6e85
Pedro Viegas <pedroasv@gmail.com>
2023-11-11 17:54:19 -0300

    chore: move all timelock related spikes to the same package

--- packages/gnosis-timelock/src/SimpleTokenVesting.sol
+++ packages/gnosis-timelock/src/SimpleTokenVesting.sol
@@ -6,38 +6,37 @@ import "@openzeppelin/contracts/finance/VestingWallet.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
 
-contract SimpleTokenVesting is ERC4626 {
+contract SimpleTokenVesting is ERC4626, VestingWallet {
 
     error SharesInVestingPeriod();
 
-    VestingWallet public vestingWallet;
     IERC20 private immutable _asset;
 
     constructor(IERC20 asset, string memory name, string memory symbol, uint64 startTimestamp, uint64 durationSeconds)
     ERC4626(asset)
     ERC20(name, symbol)
+    VestingWallet(address(this), startTimestamp, durationSeconds)
     {
         _asset = asset;
-        vestingWallet = new VestingWallet(address(this), startTimestamp, durationSeconds);
     }
 
     function totalAssets() public view virtual override returns (uint256) {
-        return _asset.balanceOf(address(vestingWallet));
+        return _asset.balanceOf(address(this));
     }
 
     function _deposit(address caller, address receiver, uint256 assets, uint256 shares) internal virtual override {
-        SafeERC20.safeTransferFrom(_asset, caller, address(vestingWallet), assets);
+        SafeERC20.safeTransferFrom(_asset, caller, address(this), assets);
         _mint(receiver, shares);
 
         emit Deposit(caller, receiver, assets, shares);
     }
 
     function _withdraw(address caller, address receiver, address owner, uint256 assets, uint256 shares) internal virtual override {
-        if (assets > vestingWallet.releasable(address(_asset))) {
+        if (assets > releasable(address(_asset))) {
             revert SharesInVestingPeriod();
         }
 
-        vestingWallet.release(address(_asset));
+        release(address(_asset));
 
         if (caller != owner) {
             _spendAllowance(owner, caller, shares);
--- packages/gnosis-timelock/test/SimpleTokenVesting.t.sol
+++ packages/gnosis-timelock/test/SimpleTokenVesting.t.sol
@@ -3,8 +3,8 @@ pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
 import "forge-std/console.sol";
-import "../contracts/SimpleTokenVesting.sol";
-import "../contracts/SimpleToken.sol";
+import "../src/SimpleTokenVesting.sol";
+import "../src/test/SimpleToken.sol";
 
 contract SimpleTokenVestingTest is Test {
   SimpleToken public token;
@@ -24,7 +24,7 @@ contract SimpleTokenVestingTest is Test {
     vault.deposit(1000, john);
 
     require(vault.totalAssets() == 1000);
-    require(token.balanceOf(address(vault.vestingWallet())) == 1000);
+    require(token.balanceOf(address(vault)) == 1000);
     require(vault.balanceOf(address(john)) == 1000);
 
     vm.warp(block.timestamp + 2);
