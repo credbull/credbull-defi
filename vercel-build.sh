44c1600e2c3ff6874e749caf594920f4b5c9286e
Ian Lucas <ian.lucas@gmail.com>
2023-11-14 15:06:44 +0100

    Working around Vercel failing build issue

---
+++ vercel-build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd packages/nextjs  # Change this path to the actual path of your @se-2/nextjs workspace
+yarn install        # Install dependencies only for this workspace
+yarn build          # Replace with your actual build command if different
+
