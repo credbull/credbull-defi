75caec47d6d5c01ac9e9bda7fc9d38f7b9b197df
Shabs219 <Shabhari@coducer.com>
2024-01-04 18:11:49 +0530

    feat: added test env variables

--- packages/app/cypress.config.ts
+++ packages/app/cypress.config.ts
@@ -1,6 +1,11 @@
+import { loadEnvConfig } from '@next/env';
 import { defineConfig } from 'cypress';
 
+const { combinedEnv } = loadEnvConfig(process.cwd());
+
+console.log(combinedEnv);
 export default defineConfig({
+  env: combinedEnv,
   e2e: {
     setupNodeEvents(_on, _config) {
       // implement node event listeners here
--- packages/app/cypress/e2e/login.cy.ts
+++ packages/app/cypress/e2e/login.cy.ts
@@ -1,5 +1,3 @@
-import { CyHttpMessages } from 'cypress/types/net-stubbing';
-
 describe('Login', () => {
   const validEmail = 'shabhari@coducer.com';
   const validPassword = 'Password@123';
@@ -12,28 +10,25 @@ describe('Login', () => {
     cy.get('a.my-Text-root.my-Anchor-root.my-al7hm1').click().should('have.attr', 'href', '/register');
 
     cy.url({ timeout: 5000 }).should('include', '/register');
-    cy.get('input#mantine-r0.my-Input-input.my-TextInput-input.my-1g14e8k')
+
+    cy.get('input[type="text"].my-Input-input.my-TextInput-input.my-1g14e8k[name="email"]')
+      .as('emailInput')
       .click()
       .type(validEmail)
       .invoke('val')
       .should('eq', validEmail);
-    cy.get('input#mantine-r1.my-13e8zn2.my-PasswordInput-innerInput')
+
+    cy.get('input[type="password"].my-13e8zn2.my-PasswordInput-innerInput[name="password"]')
+      .as('passwordInput')
       .type(validPassword)
       .should('have.value', validPassword)
       .invoke('val')
       .should('eq', validPassword);
 
-    cy.intercept(
-      'POST',
-      `${NEXT_PUBLIC_SUPABASE_URL}/auth/v1/signup?redirect_to=http%3A%2F%2Flocalhost%3A3000%2Fcode%2Fcallback/auth/v1/signup?redirect_to=http%3A%2F%2Flocalhost%3A3000%2Fcode%2Fcallback`,
-    ).as('signUpApi');
+    cy.intercept('POST', `${NEXT_PUBLIC_SUPABASE_URL}/auth/v1/signup?redirect_to*`).as('signUpApi');
 
-    cy.get('button.my-UnstyledButton-root.my-Button-root.my-t4tusc').click(); // Click on the button
-    // .should('have.attr', 'data-button', 'true');
-
-    cy.wait(2000);
-    cy.url({ timeout: 5000 }).should('include', '/login');
-    // cy.wait('@signUpApi').its('response.statusCode').should('eq', 200);
+    cy.get('button.my-UnstyledButton-root.my-Button-root.my-t4tusc').click();
+    cy.wait('@signUpApi').its('response.statusCode').should('eq', 200);
   });
 
   it('should successfully sign in with valid credentials', () => {
@@ -56,22 +51,16 @@ describe('Login', () => {
     cy.intercept('POST', `${NEXT_PUBLIC_SUPABASE_URL}/auth/v1/token?grant_type=password`).as('loginApi');
     cy.get('button.my-UnstyledButton-root.my-Button-root.my-t4tusc').click();
 
-    cy.wait(3000);
-
-    cy.url().should('include', '/dashboard');
+    cy.wait('@loginApi').then(() => {
+      cy.url().should('include', '/dashboard');
+    });
   });
 
   it('should fail sign in with inValid credentials', () => {
     cy.visit('http://localhost:3000/login');
     const invalidCredentialsErrorBody = { error: 'invalid_grant', error_description: 'Invalid login credentials' };
 
-    cy.intercept('POST', `${NEXT_PUBLIC_SUPABASE_URL}/auth/v1/token?grant_type=password`, (req) => {
-      req.reply({
-        statusCode: 400,
-        body: invalidCredentialsErrorBody,
-      });
-    }).as('failedRequest');
-
+    cy.intercept('POST', `${NEXT_PUBLIC_SUPABASE_URL}/auth/v1/token*`, (req) => {}).as('failedRequest');
     cy.get('#mantine-R3dbbb8llkq')
       // .should('be.visible', { timeout: 10000 })
       .click()
@@ -88,20 +77,22 @@ describe('Login', () => {
 
     cy.get('button.my-UnstyledButton-root.my-Button-root.my-t4tusc').click();
 
-    // cy.wait('@failedRequest').its('response.statusCode').should('eq', 400);
+    cy.wait(1000);
+
+    cy.wait('@failedRequest').its('response.statusCode').should('eq', 400);
     cy.contains('Invalid login credentials').should('be.visible');
   });
 
-  it('should sign in with discord', () => {
-    cy.visit('http://localhost:3000/login');
-    cy.url().should('include', '/login');
-    const invalidCredentialsErrorBody = { error: 'invalid_grant', error_description: 'Invalid login credentials' };
+  // it('should sign in with discord', () => {
+  //   cy.visit('http://localhost:3000/login');
+  //   cy.url().should('include', '/login');
+  //   const invalidCredentialsErrorBody = { error: 'invalid_grant', error_description: 'Invalid login credentials' };
 
-    cy.contains('button.my-UnstyledButton-root.my-Button-root', 'Discord').should('exist').click();
+  //   cy.contains('button.my-UnstyledButton-root.my-Button-root', 'Discord').should('exist').click();
 
-    cy.wait(20000);
-    cy.location('href').should('include', 'discord.com/login?redirect_to=%2Foauth2%2Fauthorize');
-  });
+  //   cy.wait(20000);
+  //   // cy.location('href').should('include', 'discord.com/login?redirect_to=%2Foauth2%2Fauthorize');
+  // });
 });
 
 export {};
