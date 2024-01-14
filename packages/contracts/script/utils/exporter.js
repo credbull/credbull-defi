1aa4b9c5c597b2b1224167588239d9199ae4f9ad
Pedro Viegas <pedroasv@gmail.com>
2024-01-14 08:02:48 +0400

    feat: only export addresses as typechain does the abi and the types

--- packages/contracts/script/utils/exporter.js
+++ packages/contracts/script/utils/exporter.js
@@ -4,7 +4,6 @@ const fs = require("fs");
 async function exportAbi() {
   const folderPath = path.resolve(__dirname, "../../broadcast");
   const outputPath = path.resolve(__dirname, "../../deployments");
-  const foundryOutPath = path.resolve(__dirname, "../../out");
 
   const deployFiles = await fs.promises.readdir(folderPath);
   for (const deployFile of deployFiles) {
@@ -17,7 +16,7 @@ async function exportAbi() {
         path.resolve(folderPath, deployFile, chainFile)
       );
 
-      let outputObj = {};
+      let addresses = {};
       for (const runFile of runFiles) {
         if (runFile.includes("latest")) {
           const runBuffer = fs.readFileSync(
@@ -31,24 +30,9 @@ async function exportAbi() {
               const contractName = tx["contractName"];
               const contractAddress = tx["contractAddress"];
 
-              const contractBuffer = fs.readFileSync(
-                path.resolve(
-                  foundryOutPath,
-                  `${contractName}.sol`,
-                  `${contractName}.json`
-                )
+              addresses[contractName] = (addresses[contractName] || []).concat(
+                contractAddress
               );
-              const output = JSON.parse(contractBuffer.toString("utf-8"));
-
-              const isNewContract = !outputObj[contractName];
-              const prop = isNewContract
-                ? contractName
-                : `${contractName}_${contractAddress}`;
-
-              outputObj[prop] = {
-                address: contractAddress,
-                abi: output.abi,
-              };
             }
           }
         }
@@ -59,9 +43,10 @@ async function exportAbi() {
 
       fs.writeFileSync(
         path.resolve(outputPath, `${chainFile}.json`),
-        JSON.stringify(outputObj)
+        JSON.stringify(addresses)
       );
     }
+    console.log(`Finished exporting addresses`);
   }
 }
 
