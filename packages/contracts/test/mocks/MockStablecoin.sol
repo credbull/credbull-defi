83ee7757e4a0cd6dba5a3e6ff594b6ba519bd7be
Krishna kumar S <krishna291098@gmail.com>
2024-01-16 09:29:35 +0530

    added comment

--- packages/contracts/test/mocks/MockStablecoin.sol
+++ packages/contracts/test/mocks/MockStablecoin.sol
@@ -5,6 +5,7 @@ import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 
+//Mock stablecoin
 contract MockStablecoin is ERC20, Ownable {
     constructor(uint256 initialSupply) ERC20("Mock USDC", "mUSDC") Ownable(msg.sender) {
         _mint(msg.sender, initialSupply);
