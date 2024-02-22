eda70a5c80dd563622ad8931d34d484c6498e701
Pedro Viegas <pedroasv@gmail.com>
2024-02-22 12:09:50 -0300

    chore: remove unused contract

--- packages/contracts/test/mocks/AKYCProvider.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.19;
-
-abstract contract AKYCProvider {
-    function status(address receiver) public view virtual returns (bool);
-
-    function updateStatus(address[] calldata _addresses, bool[] calldata _statuses) external virtual;
-}
