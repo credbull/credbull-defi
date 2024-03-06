0dab2e0217721e3206638a266e81d4243046c387
Krishna kumar S <krishna291098@gmail.com>
2024-03-06 14:52:28 +0530

    pulled latest changes

--- packages/sdk/playwright.config.ts
+++ packages/sdk/playwright.config.ts
@@ -20,10 +20,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [
-    ['html'],
-    ['list', { printSteps: true}]
-  ],
+  reporter: [['html'], ['list', { printSteps: true }]],
   // reporter: process.env.CI ? 'dot' : 'list',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
