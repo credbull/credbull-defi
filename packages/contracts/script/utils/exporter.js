c1e8c6a8b84aba14a5aa0745628764e1463242dc
Krishna kumar S <krishna291098@gmail.com>
2024-03-18 17:55:15 +0530

    added localhost url

--- packages/contracts/script/utils/exporter.js
+++ packages/contracts/script/utils/exporter.js
@@ -61,7 +61,7 @@ async function exportAddress() {
 }
 
 async function exportToSupabase(dataToStoreOnDB) {
-  const check = await fetch('http://0.0.0.0:3001/', { method: 'GET' });
+  const check = await fetch('http://127.0.0.1:3001/', { method: 'GET' });
   console.log('simple network check', await check.json());
 
   const client = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.SUPABASE_SERVICE_ROLE_KEY);
