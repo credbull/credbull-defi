948f176b44f3deecef8f2cdd5335433d1102d86b
Krishna kumar S <krishna291098@gmail.com>
2024-03-18 17:42:34 +0530

    api modules debuggin

--- packages/api/src/app.module.ts
+++ packages/api/src/app.module.ts
@@ -1,32 +1,33 @@
-import { ClassSerializerInterceptor, Module, ValidationPipe } from '@nestjs/common';
-import { APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core';
-import { ScheduleModule } from '@nestjs/schedule';
+// import { ClassSerializerInterceptor, Module, ValidationPipe } from '@nestjs/common';
+// import { APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core';
+// import { ScheduleModule } from '@nestjs/schedule';
+import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
-import { SupabaseModule } from './clients/supabase/supabase.module';
-import { AccountsModule } from './modules/accounts/accounts.module';
-import { AuthenticationModule } from './modules/authentication/authentication.module';
-import { VaultsModule } from './modules/vaults/vaults.module';
-import * as logger from './utils/logger';
+// import { SupabaseModule } from './clients/supabase/supabase.module';
+// import { AccountsModule } from './modules/accounts/accounts.module';
+// import { AuthenticationModule } from './modules/authentication/authentication.module';
+// import { VaultsModule } from './modules/vaults/vaults.module';
+// import * as logger from './utils/logger';
 import { Config } from './utils/module';
 
 @Module({
   imports: [
     Config.module(),
-    AccountsModule,
-    SupabaseModule,
-    AuthenticationModule,
-    VaultsModule,
-    ScheduleModule.forRoot(),
+    // AccountsModule,
+    // SupabaseModule,
+    // AuthenticationModule,
+    // VaultsModule,
+    // ScheduleModule.forRoot(),
   ],
   controllers: [AppController],
-  providers: [
-    { provide: APP_INTERCEPTOR, useValue: logger.interceptor },
-    { provide: APP_INTERCEPTOR, useClass: ClassSerializerInterceptor },
-    {
-      provide: APP_PIPE,
-      useValue: new ValidationPipe({ whitelist: true, transform: true }),
-    },
-  ],
+  // providers: [
+  //   { provide: APP_INTERCEPTOR, useValue: logger.interceptor },
+  //   { provide: APP_INTERCEPTOR, useClass: ClassSerializerInterceptor },
+  //   {
+  //     provide: APP_PIPE,
+  //     useValue: new ValidationPipe({ whitelist: true, transform: true }),
+  //   },
+  // ],
 })
 export class AppModule {}
