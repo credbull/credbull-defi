3e219bdff0a88119058022ca3e798fc1f9960e21
Krishna kumar S <krishna291098@gmail.com>
2024-03-18 14:46:22 +0530

    edit app url

--- packages/contracts/script/utils/exporter.js
+++ packages/contracts/script/utils/exporter.js
@@ -61,7 +61,7 @@ async function exportAddress() {
 }
 
 async function exportToSupabase(dataToStoreOnDB) {
-  const check = await fetch('http://0.0.0.0:3001/', { method: 'GET' });
+  const check = await fetch('http://172.17.0.1:3001/', { method: 'GET' });
   console.log('simple network check', await check.json());
 
   const client = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.SUPABASE_SERVICE_ROLE_KEY);
@@ -83,8 +83,6 @@ async function exportToSupabase(dataToStoreOnDB) {
     await exportAddress();
   } catch (e) {
     console.log(e);
-    const anvilCheck = await fetch('http://app:3001/', { method: 'GET' });
-    console.log('api check', await anvilCheck.json());
   } finally {
     console.log(`Finished exporting contracts`);
   }
