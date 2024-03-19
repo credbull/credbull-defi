8fb844aa1e9cdc4fcd46e9a9966f229643fd6c34
Krishna kumar S <krishna291098@gmail.com>
2024-03-19 10:07:58 +0530

    added script envs

--- packages/contracts/script/utils/exporter.js
+++ packages/contracts/script/utils/exporter.js
@@ -61,7 +61,6 @@ async function exportAddress() {
 }
 
 async function exportToSupabase(dataToStoreOnDB) {
-
   const client = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.SUPABASE_SERVICE_ROLE_KEY);
 
   const config = await client
