2cb7f7207a1972d6d4fdcdc3e0114bacad8e6670
Ian Lucas <ian.lucas@gmail.com>
2024-04-29 19:23:50 -0400

    Added Spike using Foundry Keystore for deployment

--- spikes/spike-keystore/script/Counter.s.sol
+++ spikes/spike-keystore/script/Counter.s.sol
@@ -2,11 +2,23 @@
 pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
+import "../src/Counter.sol";
 
 contract CounterScript is Script {
     function setUp() public {}
 
     function run() public {
-        vm.broadcast();
+        vm.startBroadcast();
+
+        Counter counter = new Counter();
+
+        console.logString(
+            string.concat(
+                "Counter deployed at: ", vm.toString(address(counter))
+            )
+        );
+
+        vm.stopBroadcast();
+
     }
 }
