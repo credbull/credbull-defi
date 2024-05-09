6a848c9a680bc5d90f67c2c4ec90332c39d7cf33
Ian Lucas <ian.lucas@gmail.com>
2024-05-09 16:55:58 -0400

    Examples of different toml structure for discussion

--- spikes/spike-keystore/test/TomlTest.t.sol
+++ spikes/spike-keystore/test/TomlTest.t.sol
@@ -15,51 +15,71 @@ contract TomlTest is Test {
         path = string.concat(root, "/test/fixtures/spike.toml");
     }
 
-    function test_readValue() public view {
+    function test__TomlTest_SeparateToml_ReadValue() public view {
         string memory toml = vm.readFile(path);
 
         // read a value directly
-        assertEq("FixedVault", toml.readString(".contracts.vaultName"));
+        assertEq("localChain1", toml.readString(".contracts.chainName"));
     }
 
-    function test_readTableKeys() public view {
+    function test__TomlTest_EnvSpecificTables_ReadValue() public view {
         string memory toml = vm.readFile(path);
-        string [] memory contractKeys = vm.parseTomlKeys(toml, ".contracts");
+
+        // read a value directly
+        assertEq("localChain2", toml.readString(".local.contracts.chainName"));
+    }
+
+
+    function test__TomlTest_ReadTableKeys() public view {
+
+        logKeyAndVals(".contracts"); // 1 - separate key file
+
+        logKeyAndVals(".local.contracts"); // 2 - separate env tables
+        logKeyAndVals(".local.api"); // 2 - separate env tables
+
+        // more info on syntax here: https://crates.io/crates/jsonpath-rust
+        logKeyAndVals(".env[0].contracts"); // 3 - array of env tables
+        logKeyAndVals(".env[1].contracts"); // 3 - array of env tables
+    }
+
+
+    function logKeyAndVals(string memory tomlPath) public view {
+        string memory toml = vm.readFile(path);
+        string [] memory contractKeys = vm.parseTomlKeys(toml, tomlPath);
+
+        console.log('========== logging key/vals in path: "', tomlPath, '"');
 
         for (uint256 i = 0; i < contractKeys.length; i++) {
             string memory key = contractKeys[i];
-            string memory val = toml.readString(string.concat(".contracts.", key));
+            string memory val = toml.readString(string.concat(tomlPath, ".", key));
 
             console.log(string.concat(vm.toString(i), ": ", key, "=", val));
         }
-
     }
 
 /*
-// https://book.getfoundry.sh/cheatcodes/parse-toml
-//     Represents the Contracts Table in toml file.  Recommend not to use this approach.  Binding is based on alphabetical order only!
-//
+//     Example for binding to Struct below.  Risky, don't use.  Maps using alphabetical order!
 //     From https://book.getfoundry.sh/cheatcodes/parse-toml
 //     What matters is the alphabetical order. As the JSON object is an unordered ... but the tuple is an ordered ... we had to somehow give order to the JSON.
 //     The easiest way was to order the keys by alphabetical order.
 */
     struct Contracts {
+        string chainName;
         uint256 deployerPrivateKey;
         address operatorAddress;
         address ownerAddress;
-        string vaultName;
     }
 
-    function test_ReadTableAsStruct() public view {
+    function test__TomlTest_Flat_ReadTableAsStruct() public view {
         string memory toml = vm.readFile(path);
         bytes memory contractTable = toml.parseRaw(".contracts");
 
         Contracts memory contracts = abi.decode(contractTable, (Contracts));
 
+        assertEq(toml.readString(".contracts.chainName"), contracts.chainName);
         assertEq(toml.readUint(".contracts.deployerPrivateKey"), contracts.deployerPrivateKey);
         assertEq(toml.readAddress(".contracts.operatorAddress"), contracts.operatorAddress);
         assertEq(toml.readAddress(".contracts.ownerAddress"), contracts.ownerAddress);
-        assertEq(toml.readString(".contracts.vaultName"), contracts.vaultName);
     }
 
 }
