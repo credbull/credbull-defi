b9ad4d9e409d98ab1f620cc2f9e33fc00ee7e82b
Jonathan Lodge <jplodge.pro@gmail.com>
2024-05-17 14:22:33 +0800

    Lint fixes.

--- packages/contracts/script/utils/config.js
+++ packages/contracts/script/utils/config.js
@@ -5,13 +5,13 @@ const fs = require('fs');
 const { load } = require('js-toml');
 
 module.exports = {
-   loadConfiguration: function() {
-       const env = process.env.ENVIRONMENT;
-       const configFile = path.resolve(__dirname, `../../resource/${env}.toml`);
-       console.log(`Loading configuration from: '${configFile}'`);
-       const toml = fs.readFileSync(configFile, 'utf8');
-       const config = load(toml);
-       console.log(`Loaded: '${config}'`);
-       return config;
-     }
+  loadConfiguration: function () {
+    const env = process.env.ENVIRONMENT;
+    const configFile = path.resolve(__dirname, `../../resource/${env}.toml`);
+    console.log(`Loading configuration from: '${configFile}'`);
+    const toml = fs.readFileSync(configFile, 'utf8');
+    const config = load(toml);
+    console.log(`Loaded: '${config}'`);
+    return config;
+  },
 };
