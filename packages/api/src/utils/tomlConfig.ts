bd9d299cca842db85be99440dde38776474445a6
Ian Lucas <ian.lucas@gmail.com>
2024-05-22 19:29:52 -0400

    Improve logging of errors when loading env

--- packages/api/src/utils/tomlConfig.ts
+++ packages/api/src/utils/tomlConfig.ts
@@ -40,40 +40,32 @@ export class TomlConfigService {
 
     const configFile = path.resolve(__dirname, `../../resource/api-${env}.toml`); // storing here to keep fly.io happy
 
-    console.log(`Loading configuration from: '${configFile}'`);
+    console.log(`Loading toml config from: '${configFile}'`);
 
-    let toml;
-    console.log(`!!!! 1.`);
-    console.log(`!!!! 2. ${fs.existsSync(configFile)}`);
-    try {
-      toml = fs.readFileSync(configFile, 'utf8');
-    } catch (error) {
-      console.error(`Error reading configuration file: ${error.message}`);
-      console.error(`File path: ${configFile}`);
-
-      throw Error(error);
-    }
-
-    console.log(`!!!! 1. ${fs.realpath}`);
-
+    const toml = fs.readFileSync(configFile, 'utf8');
     this.tomlConfig = load(toml) as TomlConfig;
-
-    console.log(`Here 3... '`);
 
     // include Environment into config
     this.tomlConfig.env = this.tomlConfig.env || {}; // ensure config.env exists
+
     this.tomlConfig.env.ENVIRONMENT = env;
 
-    // add secrets to the Environment
-    this.tomlConfig.secret = this.tomlConfig.secret || {}; // ensure config.env exists
-    this.tomlConfig.secret.OPERATOR_PRIVATE_KEY = new Secret(configService.getOrThrow('OPERATOR_PRIVATE_KEY'));
-    this.tomlConfig.secret.CUSTODIAN_PRIVATE_KEY = new Secret(configService.get('CUSTODIAN_PRIVATE_KEY') || '');
-    this.tomlConfig.secret.SUPABASE_SERVICE_ROLE_KEY = new Secret(
-      configService.getOrThrow('SUPABASE_SERVICE_ROLE_KEY'),
-    );
-    this.tomlConfig.secret.SUPABASE_JWT_SECRET = new Secret(configService.getOrThrow('SUPABASE_JWT_SECRET'));
-    this.tomlConfig.secret.CRON_SECRET = new Secret(configService.getOrThrow('CRON_SECRET'));
-    this.tomlConfig.secret.SLACK_TOKEN = new Secret(configService.get('SLACK_TOKEN') || '');
+    try {
+      // add secrets to the Environment
+      this.tomlConfig.secret = this.tomlConfig.secret || {}; // ensure config.env exists
+      this.tomlConfig.secret.OPERATOR_PRIVATE_KEY = new Secret(configService.getOrThrow('OPERATOR_PRIVATE_KEY'));
+      this.tomlConfig.secret.CUSTODIAN_PRIVATE_KEY = new Secret(configService.get('CUSTODIAN_PRIVATE_KEY') || '');
+      this.tomlConfig.secret.SUPABASE_SERVICE_ROLE_KEY = new Secret(
+        configService.getOrThrow('SUPABASE_SERVICE_ROLE_KEY'),
+      );
+      this.tomlConfig.secret.SUPABASE_JWT_SECRET = new Secret(configService.getOrThrow('SUPABASE_JWT_SECRET'));
+      this.tomlConfig.secret.CRON_SECRET = new Secret(configService.getOrThrow('CRON_SECRET'));
+      this.tomlConfig.secret.SLACK_TOKEN = new Secret(configService.get('SLACK_TOKEN') || '');
+    } catch (error) {
+      console.error(`Unable to load configuration: ${error.message}`);
+
+      throw Error(error);
+    }
 
     console.log('Successfully loaded configuration:', JSON.stringify(this.tomlConfig, null, 2));
   }
