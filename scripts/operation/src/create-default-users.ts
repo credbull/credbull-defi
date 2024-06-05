d31691fac04fb34105ff4c73ab12654d89baa186
Jonathan Lodge <jplodge.pro@gmail.com>
2024-06-05 20:48:46 +0800

    Fixed the schema for `Create Default Users` Removed the `wait` utility function.

--- scripts/operation/src/create-default-users.ts
+++ scripts/operation/src/create-default-users.ts
@@ -4,21 +4,15 @@ import { createUser } from './create-user';
 import { makeAdmin } from './make-admin';
 import { loadConfiguration } from './utils/config';
 
-const wait = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
-
-// NOTE (JL,2024-05-29): This Zod Schema validates the configuration for the 'Create Default Users' operation only.
-//  Meaning the config references in this module. Helper function configuration requirements are validated
-//  in situ.
+// Zod Schema for validating parameters and configuration.
 const configParser = z.object({
-  operation: z.object({
-    users: z.object({
-      admin: z.object({
-        email_address: z.string().email()
-      }),
-      bob: z.object({
-        email_address: z.string().email()
-      }),
-    })
+  users: z.object({
+    admin: z.object({
+      email_address: z.string().email()
+    }),
+    bob: z.object({
+      email_address: z.string().email()
+    }),
   }),
   secret: z.object({
     ADMIN_PASSWORD: z.string(),
@@ -35,19 +29,16 @@ const configParser = z.object({
 export const createDefaultUsers = async (config: any) => {
   configParser.parse(config)
 
-  console.log('='.repeat(100));
+  console.log('='.repeat(90));
   console.log('  Creating default Users.')
 
-  createUser(config.operation.users.admin.email_address, config.secret.ADMIN_PASSWORD!);
-  await wait(1000);
-  makeAdmin(config, config.operation.users.admin.email_address);
-  await wait(1000);
-  createUser(config.operation.users.bob.email_address, config.secret.BOB_PASSWORD!);
-  await wait(1000);
-  createUser('usera@credbull.io', 'usera123');
+  await createUser(config, config.users.admin.email_address, false, config.secret.ADMIN_PASSWORD!);
+  await makeAdmin(config, config.users.admin.email_address);
+  await createUser(config, config.users.bob.email_address, false, config.secret.BOB_PASSWORD!);
+  await createUser(config, 'usera@credbull.io', false, 'usera123');
 
   console.log('  Done.')
-  console.log('='.repeat(100));
+  console.log('='.repeat(90));
 };
 
 /**
