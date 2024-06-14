ee1319c13dd709afc38359d3335bfdafe429c5e7
Jonathan Lodge <jplodge.pro@gmail.com>
2024-06-14 21:10:36 +0800

    Converted the `admin` util functions to require the admin `User` to be passed in. Attempted a more succinct Promise Chaining style

--- packages/sdk/test/src/utils/admin.ts
+++ packages/sdk/test/src/utils/admin.ts
@@ -1,35 +1,23 @@
-import { login } from './api';
 import { Schema } from './schema';
+import { User } from './user';
 
 // Login as the Admin User and get all Vault Entities.
-export async function getVaultEntities(config: any, id: string): Promise<any> {
-  Schema.CONFIG_API_URL.merge(Schema.CONFIG_ADMIN_USER).parse(config);
+export async function getVaultEntities(config: any, admin: User, id: string): Promise<any> {
+  Schema.CONFIG_API_URL.parse(config);
 
-  return login(config, config.users.admin.email_address, config.secret.ADMIN_PASSWORD).then(
-    async ({ access_token }) => {
-      return fetch(`${config.api.url}/vaults/vault-entities/${id}`, {
-        headers: { Authorization: `Bearer ${access_token}` },
-      }).then(async (response) => {
-        return response.json();
-      });
-    },
-  );
+  return fetch(`${config.api.url}/vaults/vault-entities/${id}`, {
+    headers: { Authorization: `Bearer ${admin.accessToken}` },
+  }).then((response) => response.json());
 }
 
 // Logs in as the Admin User and whitelists the `address` for `user_id`?
-export async function whitelist(config: any, address: string, user_id: string): Promise<any> {
-  Schema.CONFIG_API_URL.merge(Schema.CONFIG_ADMIN_USER).parse(config);
+export async function whitelist(config: any, admin: User, address: string, user_id: string): Promise<any> {
+  Schema.CONFIG_API_URL.parse(config);
   Schema.ADDRESS.parse(address);
 
-  return login(config, config.users.admin.email_address, config.secret.ADMIN_PASSWORD).then(
-    async ({ access_token }) => {
-      return fetch(`${config.api.url}/accounts/whitelist`, {
-        method: 'POST',
-        body: JSON.stringify({ address, user_id }),
-        headers: { 'Content-Type': 'application/json', Authorization: `Bearer ${access_token}` },
-      }).then(async (response) => {
-        return response.json();
-      });
-    },
-  );
+  return fetch(`${config.api.url}/accounts/whitelist`, {
+    method: 'POST',
+    body: JSON.stringify({ address, user_id }),
+    headers: { 'Content-Type': 'application/json', Authorization: `Bearer ${admin.accessToken}` },
+  }).then((response) => response.json());
 }
