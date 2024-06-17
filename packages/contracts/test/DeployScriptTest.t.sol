9010c60db373ebcf257396c15ad2dddd18f10d0d
Ian Lucas <ian.lucas@gmail.com>
2024-06-17 14:08:29 -0400

    Add DeployScript tests for VaultFactory roles

--- packages/contracts/test/DeployScriptTest.t.sol
+++ packages/contracts/test/DeployScriptTest.t.sol
@@ -7,11 +7,16 @@ import { DeployVaultFactory } from "../script/DeployVaultFactory.s.sol";
 import { HelperConfig, NetworkConfig } from "../script/HelperConfig.s.sol";
 
 import { CredbullFixedYieldVault } from "../src/CredbullFixedYieldVault.sol";
+import { CredbullVaultFactory } from "../src/factories/CredbullVaultFactory.sol";
 
 import { AccessControl } from "@openzeppelin/contracts/access/AccessControl.sol";
 
 contract DeployScriptTest is Test {
     DeployVaultFactory private deployScript;
+
+    CredbullVaultFactory factory;
+    CredbullVaultFactory upsideFactory;
+
     CredbullFixedYieldVault private usdc10APYVault;
 
     HelperConfig private helperConfig;
@@ -22,15 +27,42 @@ contract DeployScriptTest is Test {
 
     function setUp() public {
         deployScript = new DeployVaultFactory();
+
+        (factory, upsideFactory,, helperConfig) = deployScript.runTest();
         (helperConfig, usdc10APYVault) = deployScript.deployVault();
 
         networkConfig = helperConfig.getNetworkConfig();
     }
 
-    function testIncredbullVaults() public view {
+    function test__DeployScriptTest_DeployVaultFactories() public view {
+        assertNotEq(address(0), address(factory), "zeroAddress - check CredbullFixedYieldVaultFactory deploy");
+        assertVaultFactoryRoles(factory);
+
+        assertNotEq(address(0), address(upsideFactory), "zeroAddress - check CredbullUpsideVaultFactory deploy");
+        assertVaultFactoryRoles(upsideFactory);
+    }
+
+    function test__DeployScriptTest_DeployVault() public view {
         assertNotEq(address(0), address(usdc10APYVault), "zeroAddress - check CredbullVault deploy");
 
         assertVaultRoles(usdc10APYVault);
+    }
+
+    function assertVaultFactoryRoles(CredbullVaultFactory vaultFactory) internal view {
+        assertHasRole(
+            vaultFactory, DEFAULT_ADMIN_ROLE, networkConfig.factoryParams.owner, "vaultFactory owner missing owner role"
+        );
+        assertHasRole(
+            vaultFactory,
+            OPERATOR_ROLE,
+            networkConfig.factoryParams.operator,
+            "vaultFactory operatorRole missing operator role"
+        ); // this is the correct impl
+
+        // ----- custodian allowed
+        assertTrue(
+            vaultFactory.isCustodianAllowed(networkConfig.factoryParams.custodian), "custodian not set on vaultFactory"
+        );
     }
 
     function assertVaultRoles(CredbullFixedYieldVault _credbullVault) internal view {
