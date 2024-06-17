bdc315fa413b736a32ece0bbbe49852350a566c1
Jonathan Lodge <jplodge.pro@gmail.com>
2024-06-17 17:21:21 +0800

    Re-enable the second test case.

--- packages/sdk/test/src/claim-yield-and-principal.spec.ts
+++ packages/sdk/test/src/claim-yield-and-principal.spec.ts
@@ -30,8 +30,6 @@ test.beforeAll('Setup', async () => {
 
   await alice.sdk.linkWallet();
   await bob.sdk.linkWallet();
-
-  console.log('SETUP COMPLETE');
 });
 
 test.describe('Claim yield and principal - Fixed', async () => {
@@ -158,122 +156,122 @@ test.describe('Claim yield and principal - Fixed', async () => {
     });
   });
 
-  // test.skip('Claim funds from Vaults with different Circle address but same Reward and Treasury addresses', async () => {
-  //   const depositAmount = BigNumber.from('1000000000');
+  test('Claim funds from Vaults with different Circle address but same Reward and Treasury addresses', async () => {
+    const depositAmount = BigNumber.from('1000000000');
 
-  //   let treasuryAddresses: string[];
-  //   let activityRewardAddresses: string[];
-  //   let treasuryPrivateKey: string[];
-  //   let activityRewardPrivateKey: string[];
+    let treasuryAddresses: string[];
+    let activityRewardAddresses: string[];
+    let treasuryPrivateKey: string[];
+    let activityRewardPrivateKey: string[];
 
-  //   await test.step('Create fixed yield vault', async () => {
-  //     const { privateKey: treasuryPkey, address: treasury } = generateAddress('treasury-test2');
-  //     const { privateKey: activityRewardPkey, address: activityReward } = generateAddress('activity_reward-test2');
-  //     await createFixedYieldVault(config, treasury, activityReward, 200);
+    await test.step('Create fixed yield vault', async () => {
+      const { privateKey: treasuryPk, address: treasury } = generateAddress('treasury-test2');
+      const { privateKey: activityRewardPk, address: activityReward } = generateAddress('activity_reward-test2');
+      await createFixedYieldVault(config, treasury, activityReward, 200);
 
-  //     const { privateKey: treasuryPkey2, address: treasury2 } = generateAddress('treasury-test2');
-  //     const { privateKey: activityRewardPkey2, address: activityReward2 } = generateAddress('activity_reward-test2');
+      const { privateKey: treasuryPk2, address: treasury2 } = generateAddress('treasury-test2');
+      const { privateKey: activityRewardPk2, address: activityReward2 } = generateAddress('activity_reward-test2');
 
-  //     await createFixedYieldVault(config, treasury2, activityReward2, 200);
+      await createFixedYieldVault(config, treasury2, activityReward2, 200);
 
-  //     treasuryAddresses = [treasury, treasury2];
-  //     activityRewardAddresses = [activityReward, activityReward2];
-  //     treasuryPrivateKey = [treasuryPkey, treasuryPkey2];
-  //     activityRewardPrivateKey = [activityRewardPkey, activityRewardPkey2];
-  //   });
+      treasuryAddresses = [treasury, treasury2];
+      activityRewardAddresses = [activityReward, activityReward2];
+      treasuryPrivateKey = [treasuryPk, treasuryPk2];
+      activityRewardPrivateKey = [activityRewardPk, activityRewardPk2];
+    });
 
-  //   vaultAddress = await test.step('Get vault and filter', async () => {
-  //     const vaults = await alice.sdk.getAllVaults();
-  //     const totalVaults = vaults.data.length;
+    vaultAddress = await test.step('Get vault and filter', async () => {
+      const vaults = await alice.sdk.getAllVaults();
+      const totalVaults = vaults.data.length;
 
-  //     expect(totalVaults).toBeGreaterThan(0);
-  //     expect(vaults).toBeTruthy();
+      expect(totalVaults).toBeGreaterThan(0);
+      expect(vaults).toBeTruthy();
 
-  //     const fixedYieldVaults = vaults.data.filter((vault: any) => vault.type === 'fixed_yield');
+      const fixedYieldVaults = vaults.data.filter((vault: any) => vault.type === 'fixed_yield');
 
-  //     //sort by created_at
-  //     fixedYieldVaults.sort((a: any, b: any) => new Date(a.created_at).getTime() - new Date(b.created_at).getTime());
-  //     expect(fixedYieldVaults).toBeTruthy();
+      //sort by created_at
+      fixedYieldVaults.sort((a: any, b: any) => new Date(a.created_at).getTime() - new Date(b.created_at).getTime());
+      expect(fixedYieldVaults).toBeTruthy();
 
-  //     return [
-  //       fixedYieldVaults[fixedYieldVaults.length - 1].address,
-  //       fixedYieldVaults[fixedYieldVaults.length - 2].address,
-  //     ];
-  //   });
+      return [
+        fixedYieldVaults[fixedYieldVaults.length - 1].address,
+        fixedYieldVaults[fixedYieldVaults.length - 2].address,
+      ];
+    });
 
-  //   await test.step('Empty custodian', async () => {
-  //     for (let i = 0; i < vaultAddress.length; i++) {
-  //       const vault = await alice.sdk.getVaultInstance(vaultAddress[i]);
-  //       const usdc = await alice.sdk.getAssetInstance(vaultAddress[i]);
-  //       const custodian = await vault.CUSTODIAN();
-  //       const custodianBalance = await usdc.balanceOf(custodian);
-  //       if (custodianBalance.gt(0)) {
-  //         await usdc.connect(testSigners.custodian.getDelegate() as Signer).transfer(TRASH_ADDRESS, custodianBalance);
-  //       }
-  //     }
-  //   });
+    await test.step('Empty custodian', async () => {
+      for (let i = 0; i < vaultAddress.length; i++) {
+        const vault = await alice.sdk.getVaultInstance(vaultAddress[i]);
+        const usdc = await alice.sdk.getAssetInstance(vaultAddress[i]);
+        const custodian = await vault.CUSTODIAN();
+        const custodianBalance = await usdc.balanceOf(custodian);
+        if (custodianBalance.gt(0)) {
+          await usdc.connect(testSigners.custodian.getDelegate()).transfer(TRASH_ADDRESS, custodianBalance);
+        }
+      }
+    });
 
-  //   await test.step('MINT USDC for user', async () => {
-  //     for (let i = 0; i < vaultAddress.length; i++) {
-  //       const vault = await alice.sdk.getVaultInstance(vaultAddress[i]);
+    await test.step('MINT USDC for user', async () => {
+      for (let i = 0; i < vaultAddress.length; i++) {
+        const vault = await alice.sdk.getVaultInstance(vaultAddress[i]);
 
-  //       await __mockMint(alice.address, depositAmount, vault, alice.testSigner.getDelegate());
-  //       await __mockMint(bob.address, depositAmount, vault, bob.testSigner.getDelegate());
-  //     }
-  //   });
+        await __mockMint(alice.address, depositAmount, vault, alice.testSigner.getDelegate());
+        await __mockMint(bob.address, depositAmount, vault, bob.testSigner.getDelegate());
+      }
+    });
 
-  //   await test.step('Approve USDC', async () => {
-  //     for (let i = 0; i < vaultAddress.length; i++) {
-  //       const usdc = await alice.sdk.getAssetInstance(vaultAddress[i]);
-  //       await usdc.connect(alice.testSigner.getDelegate()).approve(vaultAddress[i], depositAmount);
-  //       await usdc.connect(bob.testSigner.getDelegate()).approve(vaultAddress[i], depositAmount);
-  //     }
-  //   });
+    await test.step('Approve USDC', async () => {
+      for (let i = 0; i < vaultAddress.length; i++) {
+        const usdc = await alice.sdk.getAssetInstance(vaultAddress[i]);
+        await usdc.connect(alice.testSigner.getDelegate()).approve(vaultAddress[i], depositAmount);
+        await usdc.connect(bob.testSigner.getDelegate()).approve(vaultAddress[i], depositAmount);
+      }
+    });
 
-  //   await test.step('Deposit to the vault', async () => {
-  //     for (let i = 0; i < vaultAddress.length; i++) {
-  //       await alice.sdk.deposit(vaultAddress[i], depositAmount, alice.address);
-  //       await bob.sdk.deposit(vaultAddress[i], depositAmount, bob.address);
-  //     }
-  //   });
+    await test.step('Deposit to the vault', async () => {
+      for (let i = 0; i < vaultAddress.length; i++) {
+        await alice.sdk.deposit(vaultAddress[i], depositAmount, alice.address);
+        await bob.sdk.deposit(vaultAddress[i], depositAmount, bob.address);
+      }
+    });
 
-  //   await test.step('Distribute yield', async () => {
-  //     for (let i = 0; i < vaultAddress.length; i++) {
-  //       const vault = await alice.sdk.getVaultInstance(vaultAddress[i]);
-  //       const custodian = await vault.CUSTODIAN();
-  //       await __mockMint(custodian, BigNumber.from('1000000000'), vault, alice.testSigner.getDelegate());
+    await test.step('Distribute yield', async () => {
+      for (let i = 0; i < vaultAddress.length; i++) {
+        const vault = await alice.sdk.getVaultInstance(vaultAddress[i]);
+        const custodian = await vault.CUSTODIAN();
+        await __mockMint(custodian, BigNumber.from('1000000000'), vault, alice.testSigner.getDelegate());
 
-  //       const usdc = await alice.sdk.getAssetInstance(vaultAddress[i]);
+        const usdc = await alice.sdk.getAssetInstance(vaultAddress[i]);
 
-  //       //Clean up treasury and activity reward balances
-  //       const treasuryBalance = await usdc.balanceOf(treasuryAddresses[i]);
-  //       const activityRewardBalance = await usdc.balanceOf(activityRewardAddresses[i]);
+        //Clean up treasury and activity reward balances
+        const treasuryBalance = await usdc.balanceOf(treasuryAddresses[i]);
+        const activityRewardBalance = await usdc.balanceOf(activityRewardAddresses[i]);
 
-  //       const treasurySigner = signerFor(config, treasuryPrivateKey[i]);
-  //       const activityRewardSigner = signerFor(config, activityRewardPrivateKey[i]);
-  //       if (treasuryBalance.gt(0)) {
-  //         await usdc.connect(treasurySigner).transfer(TRASH_ADDRESS, treasuryBalance);
-  //       }
+        const treasurySigner = signerFor(config, treasuryPrivateKey[i]);
+        const activityRewardSigner = signerFor(config, activityRewardPrivateKey[i]);
+        if (treasuryBalance.gt(0)) {
+          await usdc.connect(treasurySigner).transfer(TRASH_ADDRESS, treasuryBalance);
+        }
 
-  //       await wait(1000);
-  //       if (activityRewardBalance.gt(0)) {
-  //         await usdc.connect(activityRewardSigner).transfer(TRASH_ADDRESS, activityRewardBalance);
-  //       }
-  //       await wait(1000);
-  //     }
+        await wait(1000);
+        if (activityRewardBalance.gt(0)) {
+          await usdc.connect(activityRewardSigner).transfer(TRASH_ADDRESS, activityRewardBalance);
+        }
+        await wait(1000);
+      }
 
-  //     await distributeFixedYieldVault(config);
+      await distributeFixedYieldVault(config);
 
-  //     for (let i = 0; i < vaultAddress.length; i++) {
-  //       const usdc = await alice.sdk.getAssetInstance(vaultAddress[i]);
-  //       const treasuryBalanceAfterDistribution = await usdc.balanceOf(treasuryAddresses[i]);
-  //       const activityRewardBalanceAfterDistribution = await usdc.balanceOf(activityRewardAddresses[i]);
+      for (let i = 0; i < vaultAddress.length; i++) {
+        const usdc = await alice.sdk.getAssetInstance(vaultAddress[i]);
+        const treasuryBalanceAfterDistribution = await usdc.balanceOf(treasuryAddresses[i]);
+        const activityRewardBalanceAfterDistribution = await usdc.balanceOf(activityRewardAddresses[i]);
 
-  //       expect(treasuryBalanceAfterDistribution.toString()).toEqual(BigNumber.from('640000000').mul(2).toString());
-  //       expect(activityRewardBalanceAfterDistribution.toString()).toEqual(
-  //         BigNumber.from('160000000').mul(2).toString(),
-  //       );
-  //     }
-  //   });
-  // });
+        expect(treasuryBalanceAfterDistribution.toString()).toEqual(BigNumber.from('640000000').mul(2).toString());
+        expect(activityRewardBalanceAfterDistribution.toString()).toEqual(
+          BigNumber.from('160000000').mul(2).toString(),
+        );
+      }
+    });
+  });
 });
