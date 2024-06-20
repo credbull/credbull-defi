898ae0d5af96361a771a0092e73c405525178fe3
Jonathan Lodge <jplodge.pro@gmail.com>
2024-06-20 19:58:20 +0800

    Added the `User` concept and support functions.

--- packages/ops/src/utils/user.ts
+++ packages/ops/src/utils/user.ts
@@ -1,9 +1,39 @@
+import { CredbullSDK } from '@credbull/sdk';
 import { SupabaseClient } from '@supabase/supabase-js';
+import { Signer } from 'ethers';
 
+import { login } from './api';
+import { signerFor } from './ethers';
 import { Schema } from './schema';
 
 // An ad-hoc Query Page Size for User Queries.
 const PAGE_SIZE = 1_000;
+
+export type User = {
+  email: string;
+  password: string;
+  id: string;
+  accessToken: string;
+  signer: Signer;
+  address: string;
+  sdk: CredbullSDK;
+};
+
+/**
+ * Utility factory function for the `User` utility type.
+ *
+ * @param config The effective configuration.
+ * @param email The User's Email address.
+ * @param password The User's Password.
+ * @param privateKey The User's Private Key.
+ */
+export async function userFor(config: any, email: string, password: string, privateKey: string): Promise<User> {
+  const { access_token: accessToken, user_id: id } = await login(config, email, password);
+  const signer = signerFor(config, privateKey);
+  const sdk = new CredbullSDK(config.api.url, { accessToken }, signer);
+
+  return { email, password, id, accessToken, signer, address: await signer.getAddress(), sdk };
+}
 
 /**
  * Searches for the `email` User, returning `undefined` if not found.
