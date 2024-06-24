7a6f684f5edfa05977492751c0190043b435f513
Jonathan Lodge <jplodge.pro@gmail.com>
2024-06-24 20:35:23 +0800

    Updated to use the CredbullSDK. Also be clearer in function

--- packages/ops/src/redeem.ts
+++ packages/ops/src/redeem.ts
@@ -2,6 +2,7 @@ import { MockStablecoin__factory } from '@credbull/contracts';
 import { formatEther, parseUnits } from 'ethers/lib/utils';
 
 import { loadConfiguration } from './utils/config';
+import { describeToken, describeVault, displayValueFor } from './utils/display';
 import { Schema } from './utils/schema';
 import { userFor } from './utils/user';
 
@@ -19,39 +20,59 @@ export async function redeem(config: any): Promise<void> {
   await userBob.sdk.linkWallet();
   console.log(' Bob signs a message and links his wallet.');
 
-  const amount = parseUnits('1000', 'mwei');
-  console.log(' Bob determines a value to play with=', amount);
+  const toRedeem = parseUnits('1000', 'mwei');
+  console.log(' Bob determines an amount of Shares to redeem=', formatEther(toRedeem));
 
   const { data: vaults } = await userBob.sdk.getAllVaults();
   console.log(' Bob queries for all available vaults=', vaults);
 
   const vaultData = vaults[0];
   const vault = await userBob.sdk.getVaultInstance(vaultData.address);
-  console.log(' Bob selects the first vault.');
+  const displayShares = displayValueFor(await vault.decimals());
+  console.log(' Bob selects the first vault=', await describeVault(vault));
 
   const usdc = await userBob.sdk.getAssetInstance(vaultData.address);
-  console.log(' Bob gets the Asset of the chosen Vault, USDC!');
+  const displayUsdc = displayValueFor(await usdc.decimals());
+  console.log(' Bob gets the Asset of the chosen Vault, USDC=', await describeToken(usdc));
+
+  console.log(` The Custodian's USDC balance is=`, displayUsdc(await usdc.balanceOf(await vault.CUSTODIAN())));
 
   const mockUsdc = MockStablecoin__factory.connect(usdc.address, userBob.signer);
-  const mintTx = await mockUsdc.mint(vault.address, amount);
+  const mintTx = await mockUsdc.mint(vault.address, toRedeem);
   await mintTx.wait();
-  console.log(' The Vault mints some USDC, using `MockStableCoin`.');
+  console.log(' Bob mints some USDC, using `MockStableCoin`, to the Vault. Minted=', displayUsdc(toRedeem));
+
+  console.log(" Bob's USDC balance is=", displayUsdc(await usdc.balanceOf(userBob.address)));
+  console.log(` The Custodian's USDC balance is=`, displayUsdc(await usdc.balanceOf(await vault.CUSTODIAN())));
 
   const shares = await vault.balanceOf(userBob.address);
-  console.log(" Bob's gets his balance of Shares in the Vault= ", shares);
+  console.log(" Bob's gets his balance of Shares in the Vault= ", displayShares(shares));
 
   const approveTx = await vault.approve(vault.address, shares);
   await approveTx.wait();
   console.log(" The Vault approves the spending of Bob's amount of shares.");
 
-  await userBob.sdk.redeem(vault.address, shares, userBob.address);
+  const userAdmin = await userFor(
+    config,
+    config.users.admin.email_address,
+    config.secret.ADMIN_PASSWORD,
+    config.secret.ADMIN_PRIVATE_KEY,
+  );
+  const adminVault = vault.connect(userAdmin.signer);
+  const windowCheckTx = await adminVault.toggleWindowCheck(false);
+  await windowCheckTx.wait();
+  const maturityCheckTx = await adminVault.toggleMaturityCheck(false);
+  await maturityCheckTx.wait();
+  console.log(' Admin disables the Vault Maturity and Window Checks');
+
+  await userBob.sdk.redeem(vault.address, toRedeem, userBob.address);
   console.log(' Bob redeems his shares from the vault.');
 
-  const balanceOfInner = await vault.balanceOf(userBob.address);
-  const balanceOfUSDC = await usdc.balanceOf(userBob.address);
+  const balanceOfShares = await vault.balanceOf(userBob.address);
+  const balanceOfUsdc = await usdc.balanceOf(userBob.address);
 
-  console.log(` Bob has ${formatEther(balanceOfInner)} sToken. - OK`);
-  console.log(` Bob has ${formatEther(balanceOfUSDC)} USDC. - OK`);
+  console.log(` Bob has ${displayShares(balanceOfShares)} ${await vault.symbol()}.`);
+  console.log(` Bob has ${displayUsdc(balanceOfUsdc)} USDC.`);
   console.log('='.repeat(80));
 }
 
