f2e283809e904db8633545fdae5923a8e7499e6e
Krishna kumar S <krishna291098@gmail.com>
2024-06-28 18:22:40 +0530

    cbl token

---
+++ packages/contracts/src/token/CBL.sol
@@ -0,0 +1,20 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+
+import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+import { ERC20Permit } from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import { ERC20Burnable } from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+
+contract CBL is ERC20, Ownable, ERC20Permit, ERC20Burnable {
+    constructor(address owner) ERC20("Credbull", "CBL") ERC20Permit("test") Ownable(owner) { }
+
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
+    }
+
+    function burn(address from, uint256 amount) external onlyOwner {
+        _burn(from, amount);
+    }
+}
