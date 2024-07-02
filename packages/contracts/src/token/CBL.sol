d7cd2df8e7feb7c2fd8da7f96dcde476fe9b7e38
Krishna kumar S <krishna291098@gmail.com>
2024-07-02 16:20:06 +0530

    CBL token deploy script and test

--- packages/contracts/src/token/CBL.sol
+++ packages/contracts/src/token/CBL.sol
@@ -6,11 +6,21 @@ import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 import { ERC20Permit } from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 import { ERC20Burnable } from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import { Pausable } from "@openzeppelin/contracts/utils/Pausable.sol";
 
-contract CBL is ERC20, Ownable, ERC20Permit, ERC20Burnable {
-    constructor(address owner) ERC20("Credbull", "CBL") ERC20Permit("test") Ownable(owner) { }
+contract CBL is ERC20, Ownable, ERC20Permit, ERC20Burnable, Pausable {
+    error CBL__MaxSupplyExceeded();
+
+    uint256 public maxSupply;
+
+    constructor(address _owner, uint256 _maxSupply) ERC20("Credbull", "CBL") ERC20Permit("test") Ownable(_owner) {
+        maxSupply = _maxSupply;
+    }
 
     function mint(address to, uint256 amount) external onlyOwner {
+        if (totalSupply() + amount > maxSupply) {
+            revert CBL__MaxSupplyExceeded();
+        }
         _mint(to, amount);
     }
 
---
+++ packages/contracts/test/token/CBLTest.sol
@@ -0,0 +1,70 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+
+import { Test } from "forge-std/Test.sol";
+import { CBL } from "../../src/token/CBL.sol";
+import { HelperConfig } from "../../script/HelperConfig.s.sol";
+import { DeployCBLToken } from "../../script/DeployCBLToken.s.sol";
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract CBLTest is Test {
+    CBL private cbl;
+    HelperConfig private helperConfig;
+    address private alice = makeAddr("alice");
+    address private owner;
+
+    function setUp() public {
+        (cbl, helperConfig) = new DeployCBLToken().runTest();
+        owner = helperConfig.getTokenParams().owner;
+    }
+
+    function test__CBL__SuccessfullyDeployCBLToken() public {
+        uint256 maxSupply = helperConfig.getTokenParams().maxSupply;
+        cbl = new CBL(owner, maxSupply);
+        assertEq(cbl.owner(), owner);
+        assertEq(cbl.maxSupply(), maxSupply);
+    }
+
+    function test__CBL__ShouldAllowOwnerToMint() public {
+        vm.prank(owner);
+        cbl.mint(alice, 100);
+    }
+
+    function test__CBL__ShouldRevertIfNotOwnerMint() public {
+        vm.prank(alice);
+        vm.expectRevert();
+        cbl.mint(alice, 100);
+    }
+
+    function test__CBL__ShouldAllowOwnerToBurn() public {
+        vm.startPrank(owner);
+        cbl.mint(owner, 100);
+        cbl.burn(owner, 100);
+        vm.stopPrank();
+    }
+
+    function test__CBL__ShouldRevertIfNotOwnerBurn() public {
+        vm.prank(owner);
+        cbl.mint(alice, 100);
+
+        vm.prank(alice);
+        vm.expectRevert();
+        cbl.burn(alice, 100);
+    }
+
+    function test__CBL__ShouldRevertIfTotalSupplyExceedsMaxSupply() public {
+        vm.startPrank(owner);
+        cbl.mint(owner, cbl.maxSupply());
+
+        vm.expectRevert(CBL.CBL__MaxSupplyExceeded.selector);
+        cbl.mint(owner, 1);
+        vm.stopPrank();
+    }
+
+    function test__CBL__ShouldReturnCorrectTotalSupply() public {
+        vm.prank(owner);
+        cbl.mint(owner, 100);
+        assertEq(cbl.totalSupply(), 100);
+    }
+}
