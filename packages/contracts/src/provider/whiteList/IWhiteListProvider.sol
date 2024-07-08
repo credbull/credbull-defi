23ab4d93cf580984bcbc9f6b8eab185cae53ff9d
Ian Lucas <ian.lucas@gmail.com>
2024-07-08 18:24:52 -0400

    Update solidity to be ^0.8.20.  This matches the OpenZeppelin v5.0.0 we are using

--- packages/contracts/src/provider/whiteList/IWhiteListProvider.sol
+++ packages/contracts/src/provider/whiteList/IWhiteListProvider.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.19;
+pragma solidity ^0.8.20;
 
 interface IWhiteListProvider {
     function status(address receiver) external view returns (bool);
--- packages/contracts/test/test/vault/SimpleWhiteListVault.t.sol
+++ packages/contracts/test/test/vault/SimpleWhiteListVault.t.sol
@@ -1,6 +1,6 @@
 //SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.19;
+pragma solidity ^0.8.20;
 
 import { Vault } from "@credbull/vault/Vault.sol";
 import { WhiteListPlugin } from "@credbull/plugin/WhiteListPlugin.sol";
