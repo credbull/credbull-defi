922b0e3505c11cfd8acaf17b967edfab5bb91430
Jonathan Lodge <jplodge.pro@gmail.com>
2024-07-18 20:10:25 +0800

    Updated to latest syntax

--- packages/contracts/script/deployer/DeployCBLToken.s.sol
+++ packages/contracts/script/deployer/DeployCBLToken.s.sol
@@ -2,46 +2,20 @@
 
 pragma solidity ^0.8.20;
 
-import { stdToml } from "forge-std/StdToml.sol";
-
 import { CBL } from "@credbull/token/CBL.sol";
 
-import { DeployedContracts } from "@script/DeployedContracts.s.sol";
-import { TokenConfigured } from "./Configured.s.sol";
+import { ConfiguredToDeployCBL } from "./Configured.s.sol";
 
-abstract contract DeployCBLToken is TokenConfigured {
-    bool private isTestMode;
-
-    function deployTo(string memory network) internal returns (CBL) {
-        return deployCBLToken(network);
-    }
-
-    function deployCBLToken(string memory network) internal returns (CBL cbl) {
-        DeployedContracts deployChecker = new DeployedContracts();
-        address owner = tokenOwner(network);
-        address minter = tokenMinter(network);
-        uint256 maxSupply = tokenMaxSupply(network);
+contract DeployCBLToken is ConfiguredToDeployCBL {
+    function run() internal returns (CBL cbl) {
+        address owner = owner();
+        address minter = minter();
+        uint256 maxSupply = maxSupply();
 
         vm.startBroadcast();
-
-        if (isTestMode || deployChecker.isDeployRequired("CBL")) {
-            cbl = new CBL(owner, minter, maxSupply);
-        }
-
+        cbl = new CBL(owner, minter, maxSupply);
         vm.stopBroadcast();
 
         return cbl;
-    }
-}
-
-contract Arbitrum is DeployCBLToken {
-    function run() external {
-        deployTo("Arbitrum");
-    }
-}
-
-contract Test is DeployCBLToken {
-    function runTest() external returns (CBL) {
-        return deployTo("Test");
     }
 }
