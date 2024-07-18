1ceacd857108ddc718ae8fe91862fc042863c751
Ian Lucas <ian.lucas@gmail.com>
2024-07-18 17:20:41 -0400

    Spike of a Batch of Vaults

---
+++ packages/contracts/src/BatchManager.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import { IERC20 } from "@openzeppelin/contracts/interfaces/IERC20.sol";
+import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+
+import { ERC4626 } from "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
+
+contract BatchManager {
+    struct BatchInfo {
+        address vaultAddress;
+        uint256 fromTimestamp;
+        uint256 toTimestamp;
+    }
+
+    BatchInfo[] public batches;
+
+    event NewBatchCreated(address indexed vaultAddress, uint256 fromTimestamp, uint256 toTimestamp);
+
+    function createNewBatch(address vaultAddress, uint256 duration) public {
+        uint256 fromTimestamp = block.timestamp;
+        uint256 toTimestamp = block.timestamp + duration;
+
+        BatchInfo memory newBatch = BatchInfo(vaultAddress, fromTimestamp, toTimestamp);
+        batches.push(newBatch);
+
+        emit NewBatchCreated(vaultAddress, fromTimestamp, toTimestamp);
+    }
+
+    function getVaultForTimestamp(uint256 timestamp) public view returns (address) {
+        for (uint256 i = 0; i < batches.length; i++) {
+            if (timestamp >= batches[i].fromTimestamp && timestamp <= batches[i].toTimestamp) {
+                return batches[i].vaultAddress;
+            }
+        }
+        revert("No vault found for the given timestamp");
+    }
+}
---
+++ packages/contracts/test/src/BatchManagerTest.t.sol
@@ -0,0 +1,69 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import { Test } from "forge-std/Test.sol";
+import { BatchManager } from "../../src/BatchManager.sol";
+
+import { IERC20 } from "@openzeppelin/contracts/interfaces/IERC20.sol";
+import { SimpleToken } from "../test/token/SimpleToken.t.sol";
+
+import { HelperConfig } from "@script/HelperConfig.s.sol";
+import { Vault } from "@credbull/vault/Vault.sol";
+import { SimpleVault } from "@test/test/vault/SimpleVault.t.sol";
+import { ParamsFactory } from "@test/test/vault/utils/ParamsFactory.t.sol";
+
+contract BatchManagerTest is Test {
+    BatchManager batchManager;
+    IERC20 token;
+
+    SimpleVault private vault;
+    HelperConfig private helperConfig;
+
+    Vault.VaultParams private vaultParams;
+
+    function setUp() public {
+        token = new SimpleToken(10000);
+        batchManager = new BatchManager();
+
+        helperConfig = new HelperConfig(true);
+        vaultParams = new ParamsFactory(helperConfig.getNetworkConfig()).createVaultParams();
+
+        vault = new SimpleVault(vaultParams);
+    }
+
+    function test__BatchManagerTest__CreateNewBatch() public {
+        uint256 duration = 1 weeks;
+
+        batchManager.createNewBatch(address(vault), duration);
+
+        // Verify that the new batch has been created
+        (address vaultAddress, uint256 fromTimestamp, uint256 toTimestamp) = batchManager.batches(0);
+        assertEq(vaultAddress, address(vault));
+        assertEq(fromTimestamp, block.timestamp);
+        assertEq(toTimestamp, block.timestamp + duration);
+    }
+
+    function test__BatchManagerTest__GetVaultForTimestamp() public {
+        uint256 duration = 1 weeks;
+        batchManager.createNewBatch(address(vault), duration);
+
+        // Advance the block timestamp
+        vm.warp(block.timestamp + 3 days);
+
+        // Verify that the correct vault is retrieved
+        address retrievedVault = batchManager.getVaultForTimestamp(block.timestamp);
+        assertEq(retrievedVault, address(vault));
+    }
+
+    function test__BatchManagerTest__GetVaultForInvalidTimestamp() public {
+        uint256 duration = 1 weeks;
+        batchManager.createNewBatch(address(vault), duration);
+
+        // Advance the block timestamp past the duration
+        vm.warp(block.timestamp + duration + 1 days);
+
+        // Expect revert when trying to retrieve a vault for an invalid timestamp
+        vm.expectRevert("No vault found for the given timestamp");
+        batchManager.getVaultForTimestamp(block.timestamp);
+    }
+}
