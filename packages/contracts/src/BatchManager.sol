ff7e8e3ef339325e58fe57faab2d1a67f2afd046
Ian Lucas <ian.lucas@gmail.com>
2024-07-18 17:24:50 -0400

    Spike of a Batch of Vaults

--- packages/contracts/src/BatchManager.sol
+++ packages/contracts/src/BatchManager.sol
@@ -1,11 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import { IERC20 } from "@openzeppelin/contracts/interfaces/IERC20.sol";
-import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
-
-import { ERC4626 } from "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
+// import { IERC20 } from "@openzeppelin/contracts/interfaces/IERC20.sol";
+// import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+// import { ERC4626 } from "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
 
 contract BatchManager {
     struct BatchInfo {
