ff57d5f6a5068ac73f331db2f3594ddba68e7534
Jonathan Lodge <jplodge.pro@gmail.com>
2024-07-19 21:03:50 +0800

    Tweaked the candeploy script Added the `deploy-alt` target to the Makefile. WIP.

--- packages/contracts/script/deployer/canDeploy.ts
+++ packages/contracts/script/deployer/canDeploy.ts
@@ -2,7 +2,7 @@ import { createClient } from '@supabase/supabase-js';
 
 import { loadConfiguration } from '../utils/config';
 
-async function canDeploy(config: any, chainId: string, contracts: string[]): Promise<boolean> {
+async function isAnyDeployed(config: any, chainId: string, contracts: string[]): Promise<boolean> {
   const supabaseClient = createClient(config.services.supabase.url, config.env.SUPABASE_SERVICE_ROLE_KEY);
 
   const { data, error } = await supabaseClient
@@ -12,11 +12,11 @@ async function canDeploy(config: any, chainId: string, contracts: string[]): Pro
     .in('contract_name', contracts)
     .is('outdated', false);
   if (error) throw error;
-  const canDeploy = data === undefined || data.length === 0;
-  if (!canDeploy) {
+  const someDeployed = data?.length > 0;
+  if (someDeployed) {
     console.log(`  Deployed Contract(s)= '${data.join(', ')}'`);
   }
-  return canDeploy;
+  return someDeployed;
 }
 
 function usage(reason: string) {
@@ -25,7 +25,8 @@ function usage(reason: string) {
   Usage: ${process.argv0} --chain-id <chain id> --contracts <contract names>
     - <chain id>: The Chain Id number
     - <contract names>: A comma-separated list of Contract Names, e.g. 'Contract1,Contract2'
-    NOTE: parameter order is exactly as above`;
+    NOTE: parameter order is exactly as above
+    Result: 0 if none of <contract names> are deployed on <chain id> network. 1 otherwise.`;
   throw new Error(msg);
 }
 
@@ -36,9 +37,8 @@ async function main() {
     const chainId = new Number(process.argv[3]) || usage(`Chain Id '${process.argv[3]}' is not a number.`);
     const contracts = process.argv[5].split(',').map((s) => s.trim());
     console.log(`Checking for contracts '${contracts.join(', ')}' on Chain '${chainId}'.`);
-    if (await canDeploy(loadConfiguration(), chainId.toString(), contracts)) {
-      exitCode = 0;
-    }
+    const noneDeployed = !(await isAnyDeployed(loadConfiguration(), chainId.toString(), contracts));
+    if (noneDeployed) exitCode = 0;
   } catch (e) {
     console.log(e);
   } finally {
