33b7738fe8801da0239435e4d5535b4151046980
Krishna kumar S <krishna291098@gmail.com>
2024-07-24 14:26:15 +0530

    fix: F-2024-4343

--- packages/contracts/src/token/CBL.sol
+++ packages/contracts/src/token/CBL.sol
@@ -7,20 +7,23 @@ import { ERC20Burnable } from "@openzeppelin/contracts/token/ERC20/extensions/ER
 import { ERC20Pausable } from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
 import { AccessControl } from "@openzeppelin/contracts/access/AccessControl.sol";
 import { ERC20Permit } from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import { ERC20Capped } from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
 
-contract CBL is ERC20, ERC20Permit, ERC20Burnable, ERC20Pausable, AccessControl {
+contract CBL is ERC20, ERC20Permit, ERC20Burnable, ERC20Pausable, ERC20Capped, AccessControl {
     error CBL__MaxSupplyExceeded();
     error CBL__ZeroAddress();
 
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
-    uint256 public immutable MAX_SUPPLY;
 
-    constructor(address _owner, address _minter, uint256 _maxSupply) ERC20("Credbull", "CBL") ERC20Permit("Credbull") {
+    constructor(address _owner, address _minter, uint256 _maxSupply)
+        ERC20("Credbull", "CBL")
+        ERC20Permit("Credbull")
+        ERC20Capped(_maxSupply)
+    {
         if (_owner == address(0) || _minter == address(0)) {
             revert CBL__ZeroAddress();
         }
 
-        MAX_SUPPLY = _maxSupply;
         _grantRole(DEFAULT_ADMIN_ROLE, _owner);
         _grantRole(MINTER_ROLE, _minter);
     }
@@ -34,14 +37,11 @@ contract CBL is ERC20, ERC20Permit, ERC20Burnable, ERC20Pausable, AccessControl 
     }
 
     function mint(address to, uint256 amount) external onlyRole(MINTER_ROLE) {
-        if (totalSupply() + amount > MAX_SUPPLY) {
-            revert CBL__MaxSupplyExceeded();
-        }
         _mint(to, amount);
     }
 
     // The following functions are overrides required by Solidity.
-    function _update(address from, address to, uint256 value) internal override(ERC20, ERC20Pausable) {
+    function _update(address from, address to, uint256 value) internal override(ERC20, ERC20Pausable, ERC20Capped) {
         super._update(from, to, value);
     }
 }
