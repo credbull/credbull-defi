42b9a7b794d64be313c7d4341ab64cd786bfec2d
Jonathan Lodge <jplodge.pro@gmail.com>
2024-07-30 16:48:23 +0800

    Restored `DeployedContracts.s.sol` deployment script. Renamed `checkDb.js` to `deployedContracts.ts` to capture it's function.

---
+++ packages/contracts/script/DeployedContracts.s.sol
@@ -0,0 +1,90 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.20;
+
+import { Script } from "forge-std/Script.sol";
+import { stdJson } from "forge-std/StdJson.sol";
+import { console2 } from "forge-std/console2.sol";
+
+/// @author ian lucas
+/// @title Helper to find previously deployed contracts.  For example, within a supabase db export.
+contract DeployedContracts is Script {
+    using stdJson for string;
+
+    string private EMPTY_STRING = "";
+
+    /// Logic to determine whether or not to deploy
+    /// @return whether to deploy or not
+    function isDeployRequired(string memory contractName) public virtual returns (bool) {
+        bool shouldDeploy = !isFoundInContractDb(contractName);
+
+        if (!shouldDeploy) {
+            console2.log("!!!!! Deployment not required for ", contractName, " !!!!!");
+        }
+
+        return shouldDeploy;
+    }
+
+    /// Read the contract Address from the database.  will revert if the contract is not found in json.
+    /// @param contractName the contractName to find
+    /// @return contract address if found, or revert
+    function getContractAddress(string memory contractName) public returns (address) {
+        string memory json = parseDeployedContracts();
+
+        if (bytes(json).length == 0) {
+            revert(
+                string.concat("Contract ", contractName, "not found in db export.  Were deploy and export successful?")
+            );
+        }
+
+        return getContractAddress(json, contractName);
+    }
+
+    /// Check if the Contract is found
+    /// @param json the json with a list of contracts or empty
+    /// @param contractName the contractName to find
+    /// @return true if the contract is found, or false
+    function isFound(string memory json, string memory contractName) public pure returns (bool) {
+        bytes memory jsonContract = json.parseRaw(string.concat(".", contractName));
+
+        return jsonContract.length > 0;
+    }
+
+    /// Read the contract Address.  will revert if the contract is not found in json.
+    /// @param json the json with a list of contracts or empty
+    /// @param contractName the contractName to find
+    /// @return contract address if found, or revert
+    function getContractAddress(string memory json, string memory contractName) public pure returns (address) {
+        address contractAddress = json.readAddress(string.concat(".", contractName));
+
+        return contractAddress;
+    }
+
+    /// Check if the Contract is found.  Uses the supabase db export.
+    /// @param contractName the contractName to find
+    /// @return true if the contract is found, or false
+    function isFoundInContractDb(string memory contractName) public returns (bool) {
+        string memory json = parseDeployedContracts();
+
+        if (bytes(json).length == 0) {
+            return false;
+        }
+
+        return isFound(json, contractName);
+    }
+
+    /// Read the deployed contracts from a supabase db export
+    /// @return json with a list of deployed contracts or EMPTY_STRING ("")
+    function parseDeployedContracts() internal returns (string memory) {
+        string memory path =
+            string.concat(vm.projectRoot(), "/script/output/deployedContracts-", vm.toString(block.chainid), ".json");
+
+        if (vm.exists(path)) {
+            string memory json = vm.readFile(path);
+
+            return json;
+        }
+
+        return EMPTY_STRING;
+    }
+}
