570cddbdc434e8c3affeef7a580ebe127a747d18
Krishna kumar S <krishna291098@gmail.com>
2024-07-30 15:15:01 +0530

    fix: F-2024-4416 only for collateral percentage

--- packages/contracts/test/src/CredbullFixedYieldVaultWithUpsideTest.t.sol
+++ packages/contracts/test/src/CredbullFixedYieldVaultWithUpsideTest.t.sol
@@ -71,6 +71,13 @@ contract CredbullFixedYieldVaultWithUpsideTest is Test {
         SimpleToken(address(cblToken)).mint(alice, 200 ether);
     }
 
+    function test__UpsideVault__ShouldRevertOnInvalidCollateralPercentage() public {
+        CredbullFixedYieldVaultWithUpside.UpsideVaultParams memory params = upsideVaultParams;
+        params.collateralPercentage = 100_01;
+        vm.expectRevert(abi.encodeWithSelector(UpsideVault.CredbullVault__InvalidCollateralPercentage.selector));
+        new CredbullFixedYieldVaultWithUpside(params);
+    }
+
     function test__UpsideVault__VaultCreationShouldRevertOnUnsupportedDecimalValue() public {
         CredbullFixedYieldVaultWithUpside.UpsideVaultParams memory params = upsideVaultParams;
         params.cblToken = new DecimalToken(1e6, 19);
