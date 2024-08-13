2fd23ceb68859718ab0376eb060d5107c3d776b0
Jonathan Lodge <jplodge.pro@gmail.com>
2024-08-13 16:23:46 +0800

    Try to force verification. Remove explicit CLI Arg with Etherscan API Key. It added nothing.

--- packages/contracts/script/utils/verifyContracts.sh
+++ packages/contracts/script/utils/verifyContracts.sh
@@ -5,5 +5,6 @@ CONTRACTS=$(jq -r '.transactions[] | select(.transactionType == "CREATE") | .con
 for CONTRACT in ${CONTRACTS} ; do
 	ADDRESS=$(jq -r --arg CT ${CONTRACT} '.transactions[] | select(.transactionType == "CREATE" and .contractName == $CT) | .contractAddress' ${TX_LOG})
 	SOURCE_FILE=$(find src test script -type f -name "${CONTRACT}*.sol" -not -name "*Test*.sol")
-	forge verify-contract ${ADDRESS} ${SOURCE_FILE}:${CONTRACT} --rpc-url ${1:?RPC_URL is required}
+	forge verify-contract ${ADDRESS} ${SOURCE_FILE}:${CONTRACT} --rpc-url ${1:?RPC_URL is required} \
+        --skip-is-verified-check --watch
 done
