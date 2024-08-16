84358ef8708cd01623a5ed55f5e86e2b8980b520
Ian Lucas <ian.lucas@gmail.com>
2024-08-16 15:43:41 -0400

    Seperate SimpleInterest and Vault from tests

--- packages/contracts/test/src/multi/SimpleInterestTest.t.sol
+++ packages/contracts/test/src/multi/SimpleInterestTest.t.sol
@@ -2,91 +2,11 @@
 pragma solidity ^0.8.23;
 
 import { Math } from "@openzeppelin/contracts/utils/math/Math.sol";
-import { Strings } from "@openzeppelin/contracts/utils/Strings.sol";
-
+import { SimpleInterest } from "./SimpleInterest.s.sol";
 import { Frequencies } from "./Frequencies.s.sol";
 
 import { Test } from "forge-std/Test.sol";
 import { console2 } from "forge-std/console2.sol";
-
-/**
- * https://en.wikipedia.org/wiki/Interest
- *
- * Simple interest is calculated only on the principal amount, or on that portion of the principal amount that remains.
- * It excludes the effect of compounding. Simple interest can be applied over a time period other than a year, for example, every month.
- *
- * Simple interest is calculated according to the following formula: (IR * P * m) / f
- * - IR is the simple annual interest rate
- * - P is the Principal (aka initial amount)
- * - m is the number of time periods elapsed
- * - f is the frequency of applying interest
- */
-contract SimpleInterest {
-    using Math for uint256;
-
-    uint256 public immutable INTEREST_RATE;
-    uint256 public immutable FREQUENCY;
-
-    uint256 public constant DECIMALS = 2;
-    uint256 public constant SCALE = 10 ** DECIMALS;
-
-    constructor(uint256 interestRatePercentage, uint256 frequency) {
-        INTEREST_RATE = interestRatePercentage;
-        FREQUENCY = frequency * SCALE;
-    }
-
-    // Interest = (IR * P * m) / f
-    function interest(uint256 principal, uint256 timePeriods) public view returns (uint256) {
-        uint256 principalScaled = principal * SCALE;
-
-        uint256 numerator = INTEREST_RATE * principalScaled * timePeriods;
-
-        uint256 simpleInterest = numerator / (FREQUENCY * SCALE);
-
-        console2.log(
-            string.concat(
-                "(IR * P * m) / f = ",
-                Strings.toString(INTEREST_RATE),
-                " * ",
-                Strings.toString(principalScaled),
-                " * ",
-                Strings.toString(timePeriods),
-                " / ",
-                Strings.toString(FREQUENCY),
-                " = ",
-                Strings.toString(simpleInterest)
-            )
-        );
-
-        return simpleInterest;
-    }
-
-    // P = Discounted / (1 - ((IR * m) / f))
-    function principalFromDiscounted(uint256 discounted, uint256 timePeriods) public view returns (uint256) {
-        uint256 interestFactor = INTEREST_RATE.mulDiv(timePeriods * SCALE, FREQUENCY); // interest rate times the number of periods (IR * m / f)
-
-        uint256 denominator = (1 * SCALE) - interestFactor; // (1 - interestFactor)
-
-        uint256 principal = discounted.mulDiv(SCALE, denominator);
-
-        console2.log(
-            string.concat(
-                "Discounted / (1 - ((IR * m) / f)) = ",
-                Strings.toString(discounted),
-                " / (1 - ((",
-                Strings.toString(INTEREST_RATE),
-                " * ",
-                Strings.toString(timePeriods),
-                " ) / ",
-                Strings.toString(FREQUENCY),
-                " = ",
-                Strings.toString(principal)
-            )
-        );
-
-        return principal;
-    }
-}
 
 contract SimpleInterestTest is Test {
     using Math for uint256;
---
+++ packages/contracts/test/src/multi/SimpleInterestVault.s.sol
@@ -0,0 +1,93 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.23;
+
+import { SimpleInterest } from "./SimpleInterest.s.sol";
+import { ERC4626 } from "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
+import { Math } from "openzeppelin-contracts/contracts/utils/math/Math.sol";
+
+import { IERC20 } from "@openzeppelin/contracts/interfaces/IERC20.sol";
+import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+import { Frequencies } from "./Frequencies.s.sol";
+
+import { console2 } from "forge-std/console2.sol";
+
+// Vault that uses SimpleInterest to calculate Shares per Asset
+// - At the start, 1 asset gives 1 share
+// - At interestFrequency 1, 1 asset gives 1 / APY shares
+// - At interestFrequency 2, 1 asset gives 1 / (2 * APY) shares,
+// - and so on...
+//
+// This is like having linear deflation over time.
+contract SimpleInterestVault is ERC4626 {
+    using Math for uint256;
+
+    SimpleInterest public simpleInterest;
+    uint256 public currentInterestFrequency = 0; // the current interest frequency
+
+    constructor(IERC20 asset, SimpleInterest _simpleInterest)
+        ERC4626(asset)
+        ERC20("Simple Interest Rate Claim", "cSIR")
+    {
+        simpleInterest = _simpleInterest;
+    }
+
+    // =============== deposit ===============
+
+    // shares that would be exchanged for the amount of assets
+    // for a given frequency of applying interest
+    // shares = assets - simpleInterest
+    function convertToSharesAtFrequency(uint256 assets, uint256 interestFrequency)
+        public
+        view
+        returns (uint256 shares)
+    {
+        if (interestFrequency == 0) return assets;
+
+        uint256 interest = simpleInterest.interest(assets, interestFrequency);
+
+        return assets - interest;
+    }
+
+    function previewDeposit(uint256 assets) public view override returns (uint256) {
+        return convertToShares(assets);
+    }
+
+    function convertToShares(uint256 assets) public view override returns (uint256) {
+        return convertToSharesAtFrequency(assets, currentInterestFrequency);
+    }
+
+    // =============== redeem ===============
+
+    // asset that would be exchanged for the amount of shares
+    // for a given frequency to calculate the non-discounted "principal" from the shares
+    // assets = principal + interest
+    function convertToAssetsAtFrequency(uint256 shares, uint256 interestFrequency)
+        public
+        view
+        returns (uint256 assets)
+    {
+        if (interestFrequency == 0) return shares;
+
+        uint256 principal = simpleInterest.principalFromDiscounted(shares, interestFrequency);
+
+        uint256 interest = simpleInterest.interest(principal, interestFrequency); // only ever give one period of interest
+
+        return principal + interest;
+    }
+
+    function previewRedeem(uint256 shares) public view override returns (uint256) {
+        return convertToAssets(shares);
+    }
+
+    function convertToAssets(uint256 shares) public view override returns (uint256) {
+        return convertToAssetsAtFrequency(shares, currentInterestFrequency);
+    }
+
+    /**
+     * @dev See {IERC4626-previewDeposit}.
+     */
+    function setCurrentInterestFrequency(uint256 _interestFrequency) public {
+        currentInterestFrequency = _interestFrequency;
+    }
+}
