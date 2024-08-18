81fedbaa6634caf7f801892adb5c66bf146fc361
Ian Lucas <ian.lucas@gmail.com>
2024-08-18 11:48:55 -0400

    Update SimpleInterestVault to be a TimelockVault

--- packages/contracts/test/src/multi/SimpleInterestVault.s.sol
+++ packages/contracts/test/src/multi/SimpleInterestVault.s.sol
@@ -2,11 +2,9 @@
 pragma solidity ^0.8.23;
 
 import { SimpleInterest } from "./SimpleInterest.s.sol";
-import { ERC4626 } from "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
 import { Math } from "openzeppelin-contracts/contracts/utils/math/Math.sol";
 
 import { IERC20 } from "@openzeppelin/contracts/interfaces/IERC20.sol";
-import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import { TimelockVault } from "./TimelockVault.s.sol";
 
 // Vault that uses SimpleInterest to calculate Shares per Asset
