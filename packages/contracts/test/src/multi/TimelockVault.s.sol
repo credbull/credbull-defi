72d84a25e685c82d60060c89a25bf97cfd9c6e18
Ian Lucas <ian.lucas@gmail.com>
2024-08-18 11:36:34 -0400

    Restrict import to fix lint

--- packages/contracts/test/src/multi/TimelockVault.s.sol
+++ packages/contracts/test/src/multi/TimelockVault.s.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import { ERC4626 } from "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract TimelockVault is ERC4626 {
     struct LockInfo {
