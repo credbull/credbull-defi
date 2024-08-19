bc84b1509bb4a118c062b2008f7c6a6f4c755d6f
Ian Lucas <ian.lucas@gmail.com>
2024-08-19 11:42:31 -0400

    Fix Scaling in SimpleInterest

--- packages/contracts/test/src/multi/SimpleInterestTest.t.sol
+++ packages/contracts/test/src/multi/SimpleInterestTest.t.sol
@@ -5,7 +5,6 @@ import { Math } from "@openzeppelin/contracts/utils/math/Math.sol";
 import { SimpleInterest } from "./SimpleInterest.s.sol";
 import { Frequencies } from "./Frequencies.s.sol";
 
-import { console2 } from "forge-std/console2.sol";
 import { Test } from "forge-std/Test.sol";
 
 contract SimpleInterestTest is Test {
