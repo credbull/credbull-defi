2be4226384f51751b7044cae9f1160bcf8461347
ChaiSomsri96 <chaisomsri96@gmail.com>
2024-08-29 19:35:05 +0700

    feat: update interface

--- spikes/spike-short-term-fixed/packages/foundry/contracts/IShortTermFixedYieldVault.sol
+++ spikes/spike-short-term-fixed/packages/foundry/contracts/IShortTermFixedYieldVault.sol
@@ -2,16 +2,9 @@
 pragma solidity ^0.8.24;
 
 interface IShortTermFixedYieldVault {
-  struct InvestmentDetails {
-    uint256 remainingLockDays;
-    uint256 depositedAmount;
-    uint256 currentYield;
-    uint256 accruedRollOverBonus;
-    uint256 withdrawalAmount;
-  }
-
+  event VaultOpened(uint256 openTime);
   event Deposited(address indexed user, uint256 amount, uint256 tokenId, uint256 time);
-  event Withdrawn(address indexed user, uint256 principal, uint256 interest, uint256 tokenId, uint256 time);
+  event Withdrawn(address indexed user, uint256 amount, uint256 tokenId, uint256 time);
 
   function openVault(uint256 _vaultOpenTime) external;
 
@@ -28,5 +21,8 @@ interface IShortTermFixedYieldVault {
   function withdraw(uint256 tokenId, uint256 amount) external;
   function withdrawMax(uint256 tokenId) external;
 
-  function getDepositInfo(uint256 tokenId) external view returns (InvestmentDetails memory);
+  function getDepositInfo(
+    uint256 tokenId
+  ) external view returns (uint256 remainingLockPeriods, uint256 currentYield, uint256 withdrawalAmount);
+  function canWithdraw(uint256 tokenId) external view returns (bool);
 }
