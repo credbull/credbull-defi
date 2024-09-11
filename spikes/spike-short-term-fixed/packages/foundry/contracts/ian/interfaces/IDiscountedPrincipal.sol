02096f771baacd67af13e8c0760db6caa71c6d10
Ian Lucas <ian.lucas@gmail.com>
2024-09-11 10:03:40 -0400

    Split Discounting out of IInterest into own interface

---
+++ spikes/spike-short-term-fixed/packages/foundry/contracts/ian/interfaces/IDiscountedPrincipal.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.20;
+
+import {IInterestMetadata} from "./IInterestMetadata.sol";
+
+/**
+ * @title IDiscountedPrincipal Interface
+ * @dev This interface provides functions to calculate "Discounted" Principal over time.
+ *
+ * @notice The `calcPrincipalFromDiscounted` and `calcDiscounted` functions are designed to be mathematical inverses of each other.
+ * This means that applying `calcPrincipalFromDiscounted` to the output of `calcDiscounted` will return the original principal amount.
+ *
+ * For example:
+ * ```
+ * uint256 originalPrincipal = 1000;
+ * uint256 discountedValue = calcDiscounted(originalPrincipal);
+ * uint256 recoveredPrincipal = calcPrincipalFromDiscounted(discountedValue);
+ * assert(recoveredPrincipal == originalPrincipal);
+ * ```
+ *
+ * This property ensures that no information is lost when discounting and then recovering the principal,
+ * making the system consistent and predictable.
+ */
+interface IDiscountedPrincipal is IInterestMetadata {
+  /**
+   * @notice Calculates the discounted principal after the elapsed time periods.
+   * @param principal The initial principal amount.
+   * @param numTimePeriodsElapsed The number of time periods for which the discount is calculated.
+   * @return discounted The discounted principal amount.
+   */
+  function calcDiscounted(uint256 principal, uint256 numTimePeriodsElapsed) external view returns (uint256 discounted);
+
+  /**
+   * @notice Recovers the original principal from a discounted value after the elapsed time periods.
+   * @param discounted The discounted principal amount.
+   * @param numTimePeriodsElapsed The number of time periods for which the discount was calculated.
+   * @return principal The recovered original principal amount.
+   */
+  function calcPrincipalFromDiscounted(uint256 discounted, uint256 numTimePeriodsElapsed) external view returns (uint256 principal);
+}
--- spikes/spike-short-term-fixed/packages/foundry/contracts/ian/interfaces/IInterest.sol
+++ spikes/spike-short-term-fixed/packages/foundry/contracts/ian/interfaces/IInterest.sol
@@ -4,21 +4,7 @@ pragma solidity ^0.8.20;
 
 /**
  * @title Interest Interface
- * @dev This interface provides functions to calculate interest and principal amounts over time.
- *
- * @notice The `calcPrincipalFromDiscounted` and `calcDiscounted` functions are designed to be mathematical inverses of each other.
- * This means that applying `calcPrincipalFromDiscounted` to the output of `calcDiscounted` will return the original principal amount.
- *
- * For example:
- * ```
- * uint256 originalPrincipal = 1000;
- * uint256 discountedValue = calcDiscounted(originalPrincipal);
- * uint256 recoveredPrincipal = calcPrincipalFromDiscounted(discountedValue);
- * assert(recoveredPrincipal == originalPrincipal);
- * ```
- *
- * This property ensures that no information is lost when discounting and then recovering the principal,
- * making the system consistent and predictable.
+ * @dev calculate interest e.g. SimpleInterest
  */
 interface IInterest {
   /**
@@ -30,31 +16,13 @@ interface IInterest {
   function calcInterest(uint256 principal, uint256 numTimePeriodsElapsed) external view returns (uint256 interest);
 
   /**
-   * @notice Calculates the discounted principal after the elapsed time periods.
+   * @notice Internal function to calculate the interest with scaling applied using the interest rate percentage.
+   * @dev - return value is scaled as Interest * SCALE.  For example: Interest=1.01 and Scale=100 returns 101
    * @param principal The initial principal amount.
-   * @param numTimePeriodsElapsed The number of time periods for which the discount is calculated.
-   * @return discounted The discounted principal amount.
+   * @param numTimePeriodsElapsed The number of time periods for which interest is calculated.
+   * @param interestRatePercentage The interest rate as a percentage.
+   * @param frequency The frequency of interest application
+   * @return interest The scaled interest amount.
    */
-  function calcDiscounted(uint256 principal, uint256 numTimePeriodsElapsed) external view returns (uint256 discounted);
-
-  /**
-   * @notice Calculates the price for a given number of periods elapsed.
-   * Price represents the accrued interest over time for a Principal of 1.
-   * @dev - return value is scaled as Price * SCALE.  For example: Price=1.01 and Scale=100 returns 101
-   * @param numTimePeriodsElapsed The number of time periods that have elapsed.
-   * @return priceScaled The price scaled by the internal scale factor.
-   */
-  function calcPriceWithScale(uint256 numTimePeriodsElapsed) external view returns (uint256 priceScaled);
-
-  /**
-   * @notice Recovers the original principal from a discounted value after the elapsed time periods.
-   * @param discounted The discounted principal amount.
-   * @param numTimePeriodsElapsed The number of time periods for which the discount was calculated.
-   * @return principal The recovered original principal amount.
-   */
-  function calcPrincipalFromDiscounted(
-    uint256 discounted,
-    uint256 numTimePeriodsElapsed
-  ) external view returns (uint256 principal);
-
+  function calcInterest(uint256 principal, uint256 numTimePeriodsElapsed, uint256 interestRatePercentage, uint256 frequency) external view returns (uint256 interest);
 }
