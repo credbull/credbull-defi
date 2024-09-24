ad698902489e820cde60ccb22f93a96f401ccc5c
ChaiSomsri96 <chaisomsri96@gmail.com>
2024-09-24 19:28:48 +0700

    feat: added events to interface

--- packages/contracts/src/interest/nawar/IMultiTokenVault.sol
+++ packages/contracts/src/interest/nawar/IMultiTokenVault.sol
@@ -8,10 +8,23 @@ import { IERC1155 } from "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
  */
 
 interface IMultiTokenVault is IERC1155 {
+    event Deposit(
+        address indexed sender, address indexed receiver, uint256 depositPeriod, uint256 assets, uint256 shares
+    );
+
+    event Withdraw(
+        address indexed sender,
+        address indexed receiver,
+        address indexed owner,
+        uint256 depositPeriod,
+        uint256 assets,
+        uint256 shares
+    );
     /**
      * @dev Returns the ERC20 underlying asset used in the vault
      * @return asset The ERC20 underlying asset
      */
+
     function getAsset() external view returns (IERC20 asset);
 
     // =============== General utility view function ===============
