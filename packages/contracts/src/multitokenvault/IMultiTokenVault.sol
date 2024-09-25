68e635040152f8911b51692fb5497542ad1bceab
ChaiSomsri96 <chaisomsri96@gmail.com>
2024-09-25 16:46:41 +0700

    feat: interface , implementation without natspec

---
+++ packages/contracts/src/multitokenvault/IMultiTokenVault.sol
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import { IERC1155 } from "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
+
+interface IMultiTokenVault is IERC1155 {
+    event Deposit(
+        address indexed sender, address indexed receiver, uint256 depositPeriod, uint256 assets, uint256 shares
+    );
+
+    event Withdraw(
+        address indexed sender,
+        address indexed receiver,
+        address indexed owner,
+        uint256 depositPeriod,
+        uint256 assets,
+        uint256 shares
+    );
+
+    /**
+     * @dev Returns the ERC20 underlying asset used in the vault
+     */
+    function asset() external view returns (address);
+
+    function currentTimePeriodsElapsed() external view returns (uint256);
+
+    function convertToSharesForDepositPeriod(uint256 assets, uint256 depositPeriod)
+        external
+        view
+        returns (uint256 shares);
+
+    function convertToAssetsForDepositPeriod(uint256 shares, uint256 depositPeriod)
+        external
+        view
+        returns (uint256 assets);
+
+    function previewRedeemForDepositPeriod(uint256 shares, uint256 depositPeriod, uint256 redeemPeriod)
+        external
+        view
+        returns (uint256 assets);
+
+    function deposit(uint256 assets, address receiver) external returns (uint256 depositPeriod, uint256 shares);
+
+    function redeemForDepositPeriod(uint256 shares, address receiver, address owner, uint256 depositPeriod)
+        external
+        returns (uint256 assets);
+
+    // =============== Operational ===============
+    /**
+     * @dev This function is for only testing purposes
+     */
+    function setCurrentTimePeriodsElapsed(uint256 currentTimePeriodsElapsed) external;
+}
---
+++ packages/contracts/src/multitokenvault/MultiTokenVault.sol
@@ -0,0 +1,70 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import { IMultiTokenVault } from "@credbull/multitokenvault/IMultiTokenVault.sol";
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import { ERC1155 } from "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
+abstract contract MultiTokenVault is IMultiTokenVault, ERC1155 {
+    using SafeERC20 for IERC20;
+
+    IERC20 private immutable _asset;
+    uint256 private _currentTimePeriodsElapsed;
+
+    constructor(IERC20 asset_) ERC1155("") {
+        _asset = asset_;
+    }
+
+    function convertToSharesForDepositPeriod(uint256 assets, uint256 depositPeriod)
+        public
+        view
+        virtual
+        returns (uint256 shares);
+
+    function convertToAssetsForDepositPeriod(uint256 shares, uint256 depositPeriod)
+        public
+        view
+        virtual
+        returns (uint256 assets);
+
+    function asset() public view virtual returns (address) {
+        return address(_asset);
+    }
+
+    function currentTimePeriodsElapsed() public view virtual returns (uint256) {
+        return _currentTimePeriodsElapsed;
+    }
+
+    function deposit(uint256 assets, address receiver) public virtual returns (uint256 depositPeriod, uint256 shares) {
+        depositPeriod = currentTimePeriodsElapsed();
+
+        // Need to fix
+        shares = convertToSharesForDepositPeriod(assets, depositPeriod);
+
+        //_deposit(_msgSender(), receiver, assets, shares);
+        // SafeERC20.safeTransferFrom(_asset, caller, address(this), assets);
+
+        _asset.safeTransferFrom(msg.sender, address(this), assets);
+        _mint(receiver, depositPeriod, shares, "");
+
+        emit Deposit(msg.sender, receiver, depositPeriod, assets, shares);
+    }
+
+    function redeemForDepositPeriod(uint256 shares, address receiver, address owner, uint256 depositPeriod)
+        public
+        virtual
+        returns (uint256 assets)
+    {
+        // Need to fix
+        assets = convertToAssetsForDepositPeriod(shares, depositPeriod);
+
+        //function _withdraw(address caller,address receiver,address owner,uint256 assets,uint256 shares)
+
+        _burn(owner, depositPeriod, shares);
+
+        _asset.safeTransfer(receiver, assets);
+
+        emit Withdraw(msg.sender, receiver, owner, depositPeriod, assets, shares);
+    }
+}
