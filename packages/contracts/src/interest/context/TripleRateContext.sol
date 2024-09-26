7a3d8298a1e31cd58fb922ec379e669c292c624e
Jonathan Lodge <jplodge.pro@gmail.com>
2024-09-26 16:32:57 +0800

    Added the `ITripleRateContext` and initial implementation.

---
+++ packages/contracts/src/interest/context/TripleRateContext.sol
@@ -0,0 +1,108 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import { Math } from "@openzeppelin/contracts/utils/math/Math.sol";
+
+import { CalcInterestMetadata } from "@credbull/interest/CalcInterestMetadata.sol";
+import { ITripleRateContext } from "@credbull/interest/context/ITripleRateContext.sol";
+
+/**
+ * @title A triple rate context, with the 'full' rate and 2 reduced rates that apply temporally across 2 tenor periods.
+ * @dev Context for yield calculations with a rate and dual reduced rates, applicable across Tenor Periods. All rate
+ *  are expressed in percentage terms and scaled using [scale()]. The 'full' rate values are encapsulated by the
+ *  [ICalcInterestMetadata].
+ */
+contract TripleRateContext is CalcInterestMetadata, ITripleRateContext {
+    using Math for uint256;
+
+    /**
+     * @notice Reverts when the Tenor Period is before the currently set Tenor Period.
+     *
+     * @param tenorPeriod The current Tenor Period.
+     * @param newTenorPeriod The attempted update Tenor Period.
+     */
+    error TripleRateContext_TenorPeriodRegressionNotAllowed(uint256 tenorPeriod, uint256 newTenorPeriod);
+
+    /**
+     * @notice Emits when the current Tenor Period is set, with its associated Reduced Rate.
+     *
+     * @param tenorPeriod The updated current Tenor Period.
+     * @param reducedRate The updated Reduced Rate for the Tenor Period.
+     */
+    event CurrentTenorPeriodAndRateChanged(uint256 tenorPeriod, uint256 reducedRate);
+
+    uint256 public immutable DEFAULT_REDUCED_RATE;
+    uint256 public immutable TENOR;
+
+    uint256 internal _currentTenorPeriod;
+    uint256 internal _currentReducedRate;
+
+    uint256 internal _previousTenorPeriod;
+    uint256 internal _previousReducedRate;
+
+    constructor(
+        uint256 fullRateInPercentageScaled_,
+        uint256 reducedRateInPercentageScaled_,
+        uint256 frequency_,
+        uint256 tenor_,
+        uint256 decimals
+    ) CalcInterestMetadata(fullRateInPercentageScaled_, frequency_, decimals) {
+        DEFAULT_REDUCED_RATE = reducedRateInPercentageScaled_;
+        TENOR = tenor_;
+
+        setReducedRateAt(1, reducedRateInPercentageScaled_);
+    }
+
+    /**
+     * @inheritdoc ITripleRateContext
+     */
+    function numPeriodsForFullRate() public view returns (uint256 numPeriods) {
+        return TENOR;
+    }
+
+    /**
+     * @inheritdoc ITripleRateContext
+     */
+    function currentTenorPeriodAndRate()
+        external
+        view
+        override
+        returns (uint256 currentTenorPeriod, uint256 reducedRateInPercentageScaled)
+    {
+        currentTenorPeriod = _currentTenorPeriod;
+        reducedRateInPercentageScaled = _currentReducedRate;
+    }
+
+    /**
+     * @inheritdoc ITripleRateContext
+     */
+    function previousTenorPeriodAndRate()
+        external
+        view
+        override
+        returns (uint256 previousTenorPeriod, uint256 reducedRateInPercentageScaled)
+    {
+        previousTenorPeriod = _previousTenorPeriod;
+        reducedRateInPercentageScaled = _previousReducedRate;
+    }
+
+    /**
+     * @notice Mutator function to set the current Tenor Period and its associated Reduced Rate.
+     * @dev Reverts with [TripleRateContext_PeriodRegressionNotAllowed] if `tenorPeriod_` is before the
+     *  current Tenor Period.
+     *
+     * @param tenorPeriod_ The [uint256] Tenor Period at which to set the associated Rate.
+     * @param reducedRateScaled_ The [uint256] Reduced Rate scaled percentage value.
+     */
+    function setReducedRateAt(uint256 tenorPeriod_, uint256 reducedRateScaled_) public {
+        if (tenorPeriod_ <= _currentTenorPeriod) {
+            revert TripleRateContext_TenorPeriodRegressionNotAllowed(_currentTenorPeriod, tenorPeriod_);
+        }
+
+        _previousTenorPeriod = _currentTenorPeriod;
+        _previousReducedRate = _currentReducedRate;
+
+        _currentTenorPeriod = tenorPeriod_;
+        _currentReducedRate = reducedRateScaled_;
+    }
+}
--- packages/contracts/src/strategy/MultipleRateYieldStrategy.sol
+++ packages/contracts/src/strategy/MultipleRateYieldStrategy.sol
@@ -3,13 +3,13 @@ pragma solidity ^0.8.20;
 
 import { IMultipleRateContext } from "@credbull/interest/context/IMultipleRateContext.sol";
 import { CalcSimpleInterest } from "@credbull/interest/CalcSimpleInterest.sol";
-import { IYieldStrategy } from "@credbull/strategy/IYieldStrategy.sol";
+import { AbstractYieldStrategy } from "@credbull/strategy/AbstractYieldStrategy.sol";
 
 /**
  * @title MultipleRateYieldStrategy
  * @dev Calculates returns using different rates depending on the holding period.
  */
-contract MultipleRateYieldStrategy is IYieldStrategy {
+contract MultipleRateYieldStrategy is AbstractYieldStrategy {
     /**
      * @notice Returns the yield for `principal` from `fromPeriod` to `toPeriod` using full and reduced rates.
      * @dev Reverts with [IYieldStrategy_InvalidContextAddress] if `contextContract` is invalid.
@@ -79,45 +79,6 @@ contract MultipleRateYieldStrategy is IYieldStrategy {
         return CalcSimpleInterest.calcPriceFromInterest(
             numPeriodsElapsed, context.rateScaled(), context.frequency(), context.scale()
         );
-    }
-
-    /**
-     * @dev Utility function to calculate the Number Of Periods between `from_` and `to_`. Also reduces Stack Depth in
-     *  invoking functions.
-     *
-     * @param from_ The from period
-     * @param to_ The to period
-     */
-    function _noOfPeriods(uint256 from_, uint256 to_) internal pure returns (uint256) {
-        return to_ - from_;
-    }
-
-    /**
-     * @dev Calculates the number of Full Rate Periods.
-     *
-     * @param _noOfPeriodsForFullRate  The number of periods that apply for Full Rate.
-     * @param _from The from period
-     * @param _to The to period
-     * @return The calculated number of Full Rate Periods.
-     */
-    function _noOfFullRatePeriods(uint256 _noOfPeriodsForFullRate, uint256 _from, uint256 _to)
-        internal
-        pure
-        returns (uint256)
-    {
-        uint256 _periods = _noOfPeriods(_from, _to);
-        return _periods - (_periods % _noOfPeriodsForFullRate);
-    }
-
-    /**
-     * @dev Calculates the first Reduced Rate Period.
-     *
-     * @param noOfFullRatePeriods_  The number of Full Rate Periods
-     * @param _from  The from period.
-     * @return The calculated first Reduced Rate Period.
-     */
-    function _firstReducedRatePeriod(uint256 noOfFullRatePeriods_, uint256 _from) internal pure returns (uint256) {
-        return noOfFullRatePeriods_ != 0 ? _from + noOfFullRatePeriods_ : _from;
     }
 
     /**
