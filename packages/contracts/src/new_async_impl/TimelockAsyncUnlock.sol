74ffcc78d156cbe8b85a87785db66859b3886c32
ChaiSomsri96 <chaisomsri96@gmail.com>
2024-10-02 18:09:59 +0700

    feat: add maxRequestUnlock condition in requestUnlock

--- packages/contracts/src/new_async_impl/TimelockAsyncUnlock.sol
+++ packages/contracts/src/new_async_impl/TimelockAsyncUnlock.sol
@@ -4,22 +4,26 @@ pragma solidity ^0.8.20;
 import { ITimelockAsyncUnlock } from "@credbull/new_async_impl/ITimelockAsyncUnlock.sol";
 
 abstract contract TimelockAsyncUnlock is ITimelockAsyncUnlock {
-    struct UnlockItem {
-        address account;
-        uint256 depositPeriod;
-        uint256 unlockPeriod;
-        uint256 amount;
-    }
-
-    // mapping(uint256 depositPeriod => mapping(address account => UnlockItem)) private _unlockRequests;
-
-    mapping(uint256 depositPeriod => mapping(address account => uint256)) private _unlockRequests1;
-    mapping(uint256 depositPeriod => mapping(address account => mapping(uint256 unlockPeriod => uint256))) private
-        _unlockRequests2;
+    mapping(uint256 depositPeriod => mapping(address account => uint256 amount)) private _unlockRequests;
+    mapping(uint256 depositPeriod => mapping(address account => mapping(uint256 unlockPeriod => uint256 amount)))
+        private _unlockRequestsByUnlockPeriod;
 
     uint256 private _noticePeriod;
 
-    error TimelockAsyncUnlock__ExceededMaxUnlock();
+    error TimelockAsyncUnlock__ExceededMaxUnlock(address owner, uint256 amount, uint256 unlockRequestedAmount);
+
+    modifier validateUnlockPeriod(uint256 depositPeriod, uint256 unlockPeriod) {
+        if (unlockPeriod < depositPeriod) {
+            revert();
+        }
+
+        // Need to check with Ian
+        if (unlockPeriod >= currentPeriod()) {
+            revert();
+        }
+
+        _;
+    }
 
     constructor(uint256 noticePeriod_) {
         _noticePeriod = noticePeriod_;
@@ -45,7 +49,7 @@ abstract contract TimelockAsyncUnlock is ITimelockAsyncUnlock {
     function lockedAmount(address owner, uint256 depositPeriod) public view virtual returns (uint256 lockedAmount_);
 
     function unlockRequested(address owner, uint256 depositPeriod) public view virtual returns (uint256) {
-        return _unlockRequests1[depositPeriod][owner];
+        return _unlockRequests[depositPeriod][owner];
     }
 
     function maxRequestUnlock(address owner, uint256 depositPeriod) public view virtual returns (uint256) {
@@ -55,29 +59,39 @@ abstract contract TimelockAsyncUnlock is ITimelockAsyncUnlock {
      * @dev Caller should be owner or approved user for owner
      */
 
-    function requestUnlock(address owner, uint256 amount, uint256 depositPeriod) public virtual
-    returns (uint256 unlockPeriod)
+    function requestUnlock(address owner, uint256 amount, uint256 depositPeriod)
+        public
+        virtual
+        returns (uint256 unlockPeriod)
     {
         /**
          * Need to check if msg.sender = owner or isApprovedForAll(owner, msg.sender) in multitoken vault
          */
-         unlockPeriod = currentUnlockPeriod();
+        if (maxRequestUnlock(owner, depositPeriod) < amount) {
+            revert();
+        }
 
-        _unlockRequests1[depositPeriod][owner] += amount;
-        _unlockRequests2[depositPeriod][owner][unlockPeriod] += amount;
+        unlockPeriod = currentUnlockPeriod();
+
+        _unlockRequests[depositPeriod][owner] += amount;
+        _unlockRequestsByUnlockPeriod[depositPeriod][owner][unlockPeriod] += amount;
     }
 
     /**
      * @dev every one can call this unlock function
      */
-    function unlock(address owner, uint256 depositPeriod, uint256 unlockPeriod, uint256 amount) public virtual {
-        uint256 unlockRequestedAmount = _unlockRequests2[depositPeriod][owner][unlockPeriod];
+    function unlock(address owner, uint256 depositPeriod, uint256 unlockPeriod, uint256 amount)
+        public
+        virtual
+        validateUnlockPeriod(depositPeriod, unlockPeriod)
+    {
+        uint256 unlockRequestedAmount = _unlockRequestsByUnlockPeriod[depositPeriod][owner][unlockPeriod];
 
         if (amount > unlockRequestedAmount) {
-            revert TimelockAsyncUnlock__ExceededMaxUnlock();
+            revert TimelockAsyncUnlock__ExceededMaxUnlock(owner, amount, unlockRequestedAmount);
         }
 
-        _unlockRequests2[depositPeriod][owner][unlockPeriod] = unlockRequestedAmount - amount;
-        _unlockRequests1[depositPeriod][owner] -= amount;
+        _unlockRequestsByUnlockPeriod[depositPeriod][owner][unlockPeriod] = unlockRequestedAmount - amount;
+        _unlockRequests[depositPeriod][owner] -= amount;
     }
 }
