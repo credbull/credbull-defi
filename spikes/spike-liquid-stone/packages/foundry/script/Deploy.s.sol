f5023adcac6b522d7a799c2b46c4cd7e629e8a04
nawar-hisso <nawarhisso@gmail.com>
2024-10-08 01:43:06 +0400

    :bug: Fix Remapping

--- spikes/spike-liquid-stone/packages/foundry/script/Deploy.s.sol
+++ spikes/spike-liquid-stone/packages/foundry/script/Deploy.s.sol
@@ -3,8 +3,11 @@ pragma solidity ^0.8.19;
 
 import "./DeployHelpers.s.sol";
 import "../contracts/SimpleUSDC.sol";
+import "@credbull/yield/LiquidContinuousMultiTokenVault.sol";
+import "@credbull/yield/strategy/TripleRateYieldStrategy.sol";
 
 import { IERC20Metadata } from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
+import { RedeemOptimizerFIFO } from "@credbull/token/ERC1155/RedeemOptimizerFIFO.sol";
 
 import { TomlConfig } from "@script/TomlConfig.s.sol";
 import { stdToml } from "forge-std/StdToml.sol";
@@ -39,6 +42,7 @@ contract DeployScript is ScaffoldETHDeploy, TomlConfig {
         vm.startBroadcast(deployerPrivateKey);
 
         SimpleUSDC simpleUSDC = _deploySimpleUSDC();
+        LiquidContinuousMultiTokenVault vault = _deployLiquidContinuousVault(simpleUSDC);
 
         vm.stopBroadcast();
 
@@ -58,9 +62,62 @@ contract DeployScript is ScaffoldETHDeploy, TomlConfig {
         return simpleUSDC;
     }
 
+    function _deployLiquidContinuousVault(SimpleUSDC simpleUSDC) internal returns (LiquidContinuousMultiTokenVault) {
+        uint256 SCALE = 10 ** simpleUSDC.decimals();
+
+        uint256 fullRateBasisPoints = _readUintFromToml(".full_rate_bps");
+        uint256 reducedRateBasisPoints = _readUintFromToml(".reduced_rate_bps");
+
+        TripleRateContext.ContextParams memory contextParams = TripleRateContext.ContextParams({
+            fullRateScaled: fullRateBasisPoints * SCALE / 100,
+            initialReducedRate: ITripleRateContext.PeriodRate({
+                interestRate: reducedRateBasisPoints * SCALE / 100,
+                effectiveFromPeriod: 0
+            }),
+            frequency: 365,
+            tenor: 30,
+            decimals: simpleUSDC.decimals()
+        });
+
+        uint256 startTimestamp = _readUintWithDefault(tomlConfig, ".vault_start_timestamp", block.timestamp);
+
+        LiquidContinuousMultiTokenVault.VaultAuth memory vaultAuth = LiquidContinuousMultiTokenVault.VaultAuth({
+            owner: owner,
+            operator: operator,
+            upgrader: owner
+        });
+
+        LiquidContinuousMultiTokenVault.VaultParams memory params = LiquidContinuousMultiTokenVault.VaultParams({
+            vaultAuth: vaultAuth,
+            asset: simpleUSDC,
+            yieldStrategy: _deployYieldStrategy(),
+            redeemOptimizer: _deployRedeemOptimizer(),
+            vaultStartTimestamp: startTimestamp,
+            redeemNoticePeriod: 1,
+            contextParams: contextParams
+        });
+
+        LiquidContinuousMultiTokenVault vault = new LiquidContinuousMultiTokenVault();
+        vault.initialize(params);
+        return vault;
+    }
+
+    function _deployYieldStrategy() internal returns (IYieldStrategy) {
+        return new TripleRateYieldStrategy();
+    }
+
+    function _deployRedeemOptimizer() internal returns (IRedeemOptimizer) {
+        return new RedeemOptimizerFIFO(IRedeemOptimizer.OptimizerBasis.Shares, 0);
+    }
+
     function _mintUserTokens(SimpleUSDC simpleUSDC, address _owner) internal {
         uint256 SCALE = 10 ** simpleUSDC.decimals();
         uint256 userMintAmount = 100_000 * SCALE;
         simpleUSDC.mint(_owner, userMintAmount);
+    }
+
+    function _readUintFromToml(string memory keySuffix) internal view returns (uint256) {
+        string memory contractKey = ".evm.contracts.liquid_continuous_multi_token_vault";
+        return tomlConfig.readUint(string.concat(contractKey, keySuffix));
     }
 }
