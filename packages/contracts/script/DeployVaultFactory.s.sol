53d46a20c6493ce5c208477764766c30a1de75d4
nawar-hisso <nawarhisso@gmail.com>
2024-10-08 13:09:57 +0400

    :zap: Implement Deployment & ABI Generation Under The Main Contracts Structure

--- packages/contracts/script/DeployVaultFactory.s.sol
+++ packages/contracts/script/DeployVaultFactory.s.sol
@@ -6,39 +6,44 @@ import { Script } from "forge-std/Script.sol";
 import { console2 } from "forge-std/console2.sol";
 
 import { HelperConfig, NetworkConfig } from "@script/HelperConfig.s.sol";
-
+import { SimpleUSDC } from "@test/test/token/SimpleUSDC.t.sol";
 import { CredbullFixedYieldVaultFactory } from "@credbull/CredbullFixedYieldVaultFactory.sol";
 import { CredbullUpsideVaultFactory } from "@credbull/CredbullUpsideVaultFactory.sol";
 import { CredbullWhiteListProvider } from "@credbull/CredbullWhiteListProvider.sol";
+import { IYieldStrategy } from "@credbull/yield/strategy/IYieldStrategy.sol";
+import { TripleRateYieldStrategy } from "@credbull/yield/strategy/TripleRateYieldStrategy.sol";
+import { ITripleRateContext } from "@credbull/yield/context/ITripleRateContext.sol";
+import { TripleRateContext } from "@credbull/yield/context/TripleRateContext.sol";
+import { IRedeemOptimizer } from "@credbull/token/ERC1155/IRedeemOptimizer.sol";
+import { RedeemOptimizerFIFO } from "@credbull/token/ERC1155/RedeemOptimizerFIFO.sol";
+import { LiquidContinuousMultiTokenVault } from "@credbull/yield/LiquidContinuousMultiTokenVault.sol";
+import { IERC20Metadata } from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
+import { ERC1967Proxy } from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 
 import { DeployedContracts } from "./DeployedContracts.s.sol";
 
 contract DeployVaultFactory is Script {
     bool private isTestMode;
 
-    function runTest()
-        public
-        returns (
-            CredbullFixedYieldVaultFactory factory,
-            CredbullUpsideVaultFactory upsideFactory,
-            CredbullWhiteListProvider whiteListProvider,
-            HelperConfig helperConfig
-        )
-    {
+    CredbullFixedYieldVaultFactory public factory;
+    CredbullUpsideVaultFactory public upsideFactory;
+    CredbullWhiteListProvider public whiteListProvider;
+
+    struct VaultDeploymentResult {
+        CredbullFixedYieldVaultFactory factory;
+        CredbullUpsideVaultFactory upsideFactory;
+        CredbullWhiteListProvider whiteListProvider;
+        HelperConfig helperConfig;
+        LiquidContinuousMultiTokenVault vault;
+    }
+
+    function runTest() public returns (VaultDeploymentResult memory) {
         isTestMode = true;
         return run();
     }
 
-    function run()
-        public
-        returns (
-            CredbullFixedYieldVaultFactory factory,
-            CredbullUpsideVaultFactory upsideFactory,
-            CredbullWhiteListProvider whiteListProvider,
-            HelperConfig helperConfig
-        )
-    {
-        helperConfig = new HelperConfig(isTestMode);
+    function run() public returns (VaultDeploymentResult memory) {
+        HelperConfig helperConfig = new HelperConfig(isTestMode);
         NetworkConfig memory config = helperConfig.getNetworkConfig();
 
         address owner = config.factoryParams.owner;
@@ -65,8 +70,99 @@ contract DeployVaultFactory is Script {
             console2.log("!!!!! Deploying CredbullWhiteListProvider !!!!!");
         }
 
+        LiquidContinuousMultiTokenVault vault = _deployVaultContracts(owner, operator);
+
         vm.stopBroadcast();
 
-        return (factory, upsideFactory, whiteListProvider, helperConfig);
+        return VaultDeploymentResult({
+            factory: factory,
+            upsideFactory: upsideFactory,
+            whiteListProvider: whiteListProvider,
+            helperConfig: helperConfig,
+            vault: vault
+        });
+    }
+
+    // New function to handle the vault contracts deployment
+    function _deployVaultContracts(address owner, address operator)
+        internal
+        returns (LiquidContinuousMultiTokenVault)
+    {
+        SimpleUSDC simpleUSDC = new SimpleUSDC(owner, type(uint128).max);
+        console2.log(string.concat("!!!!! Deploying SimpleUSDC [", vm.toString(address(simpleUSDC)), "] !!!!!"));
+
+        IYieldStrategy yieldStrategy = new TripleRateYieldStrategy();
+        console2.log(string.concat("!!!!! Deploying IYieldStrategy [", vm.toString(address(yieldStrategy)), "] !!!!!"));
+
+        IRedeemOptimizer redeemOptimizer = new RedeemOptimizerFIFO(IRedeemOptimizer.OptimizerBasis.Shares, 0);
+        console2.log(
+            string.concat("!!!!! Deploying IRedeemOptimizer [", vm.toString(address(redeemOptimizer)), "] !!!!!")
+        );
+
+        LiquidContinuousMultiTokenVault.VaultAuth memory vaultAuth =
+            LiquidContinuousMultiTokenVault.VaultAuth({ owner: owner, operator: operator, upgrader: owner });
+
+        LiquidContinuousMultiTokenVault.VaultParams memory vaultParams =
+            _createVaultParams(vaultAuth, simpleUSDC, yieldStrategy, redeemOptimizer);
+
+        LiquidContinuousMultiTokenVault liquidVaultImpl = new LiquidContinuousMultiTokenVault();
+        console2.log(
+            string.concat(
+                "!!!!! Deploying LiquidContinuousMultiTokenVault Implementation [",
+                vm.toString(address(liquidVaultImpl)),
+                "] !!!!!"
+            )
+        );
+
+        liquidVaultImpl.initialize(vaultParams);
+
+        ERC1967Proxy liquidVaultProxy = new ERC1967Proxy(
+            address(liquidVaultImpl), abi.encodeWithSelector(liquidVaultImpl.initialize.selector, vaultParams)
+        );
+        console2.log(
+            string.concat(
+                "!!!!! Deploying LiquidContinuousMultiTokenVault Proxy [",
+                vm.toString(address(liquidVaultProxy)),
+                "] !!!!!"
+            )
+        );
+
+        return LiquidContinuousMultiTokenVault(address(liquidVaultProxy));
+    }
+
+    function _createVaultParams(
+        LiquidContinuousMultiTokenVault.VaultAuth memory vaultAuth,
+        IERC20Metadata asset,
+        IYieldStrategy yieldStrategy,
+        IRedeemOptimizer redeemOptimizer
+    ) public view returns (LiquidContinuousMultiTokenVault.VaultParams memory vaultParams_) {
+        uint256 fullRateBasisPoints = 1000;
+        uint256 reducedRateBasisPoints = 550;
+        uint256 startTimestamp = block.timestamp;
+
+        uint256 scale = 10 ** asset.decimals();
+
+        TripleRateContext.ContextParams memory contextParams = TripleRateContext.ContextParams({
+            fullRateScaled: fullRateBasisPoints * scale / 100,
+            initialReducedRate: ITripleRateContext.PeriodRate({
+                interestRate: reducedRateBasisPoints * scale / 100,
+                effectiveFromPeriod: 0
+            }),
+            frequency: 360,
+            tenor: 30,
+            decimals: asset.decimals()
+        });
+
+        LiquidContinuousMultiTokenVault.VaultParams memory vaultParams = LiquidContinuousMultiTokenVault.VaultParams({
+            vaultAuth: vaultAuth,
+            asset: asset,
+            yieldStrategy: yieldStrategy,
+            redeemOptimizer: redeemOptimizer,
+            vaultStartTimestamp: startTimestamp,
+            redeemNoticePeriod: 1,
+            contextParams: contextParams
+        });
+
+        return vaultParams;
     }
 }
