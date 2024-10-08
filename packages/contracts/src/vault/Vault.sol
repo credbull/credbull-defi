8326483a4032fcfd54caa706415eaf104fd5af5d
Krishna kumar S <krishna291098@gmail.com>
2024-10-08 16:41:41 +0530

    Oz mapping build fix (#129)
    * upgraded oz to v5.0
    
    * fixed math lib path

--- packages/contracts/src/vault/Vault.sol
+++ packages/contracts/src/vault/Vault.sol
@@ -6,7 +6,7 @@ import { ERC4626 } from "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.
 import { IERC20 } from "@openzeppelin/contracts/interfaces/IERC20.sol";
 import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import { Math } from "openzeppelin-contracts/contracts/utils/math/Math.sol";
+import { Math } from "@openzeppelin/contracts/utils/math/Math.sol";
 import { Pausable } from "@openzeppelin/contracts/utils/Pausable.sol";
 
 /**
