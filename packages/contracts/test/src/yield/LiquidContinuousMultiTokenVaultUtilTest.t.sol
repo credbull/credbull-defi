9cb8cb7433db2b3e5833a7476f12df7c9f02c1dc
nawar-hisso <nawarhisso@gmail.com>
2024-10-14 13:42:51 +0400

    :white_check_mark: Fix The Test Case

--- packages/contracts/test/src/yield/LiquidContinuousMultiTokenVaultUtilTest.t.sol
+++ packages/contracts/test/src/yield/LiquidContinuousMultiTokenVaultUtilTest.t.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.20;
 import { LiquidContinuousMultiTokenVault } from "@credbull/yield/LiquidContinuousMultiTokenVault.sol";
 import { TripleRateYieldStrategy } from "@credbull/yield/strategy/TripleRateYieldStrategy.sol";
 import { IMultiTokenVault } from "@credbull/token/ERC1155/IMultiTokenVault.sol";
+import { MultiTokenVault } from "@credbull/token/ERC1155/MultiTokenVault.sol";
 import { IRedeemOptimizer } from "@credbull/token/ERC1155/IRedeemOptimizer.sol";
 import { RedeemOptimizerFIFO } from "@credbull/token/ERC1155/RedeemOptimizerFIFO.sol";
 import { Timer } from "@credbull/timelock/Timer.sol";
@@ -114,6 +115,16 @@ contract LiquidContinuousMultiTokenVaultUtilTest is LiquidContinuousMultiTokenVa
         _liquidVault.requestUnlock(alice, _asSingletonArray(testParams.depositPeriod), _asSingletonArray(shares));
 
         _warpToPeriod(_liquidVault, testParams.redeemPeriod);
+
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                MultiTokenVault.MultiTokenVault__CallerMissingApprovalForAll.selector, address(this), alice
+            )
+        );
+        _liquidVault.redeemForDepositPeriod(shares, alice, alice, testParams.depositPeriod, testParams.redeemPeriod);
+
+        vm.prank(alice);
+        _liquidVault.setApprovalForAll(address(this), true);
 
         vm.expectRevert(PausableUpgradeable.EnforcedPause.selector);
         _liquidVault.redeemForDepositPeriod(shares, alice, alice, testParams.depositPeriod, testParams.redeemPeriod);
