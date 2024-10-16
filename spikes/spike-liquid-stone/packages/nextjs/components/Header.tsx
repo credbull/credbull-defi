99bb4184017b0f112f15f8e9e47172a46db19376
nawar-hisso <nawarhisso@gmail.com>
2024-10-16 18:58:30 +0400

    :bug: Fix Header

--- spikes/spike-liquid-stone/packages/nextjs/components/Header.tsx
+++ spikes/spike-liquid-stone/packages/nextjs/components/Header.tsx
@@ -109,8 +109,8 @@ export const Header = () => {
             />
           </div>
           <div className="flex flex-col">
-            <span className="font-bold leading-tight">Scaffold-ETH</span>
-            <span className="text-xs">Ethereum dev stack</span>
+            <span className="font-bold leading-tight">CredBull</span>
+            <span className="text-xs">reDeFining Private Credit</span>
           </div>
         </Link>
         <ul className="hidden lg:flex lg:flex-nowrap menu menu-horizontal px-1 gap-2">
