c2412c46ae83462733e4b36354d51a95ca649823
ChaiSomsri96 <chaisomsri96@gmail.com>
2024-10-23 16:04:37 +0700

    feat: unit tests update

--- packages/contracts/test/src/timelock/TimelockAsyncUnlockV2Test.t.sol
+++ packages/contracts/test/src/timelock/TimelockAsyncUnlockV2Test.t.sol
@@ -16,42 +16,49 @@ contract TimelockAsyncUnlockTest is Test {
      *      making it adaptable for testing various derived contracts.
      */
     address internal asyncUnlock;
-
     uint256 internal noticePeriod;
+
+    address internal alice = makeAddr("alice");
+    address internal bob = makeAddr("bob");
 
     function setUp() public {
         noticePeriod = 1;
-
         asyncUnlock = _deployTimelockAsyncUnlock(noticePeriod);
     }
 
-    function testFuzz__TimelockAsyncUnlock__LockAmount(uint256 depositPeriod, uint256 lockAmount) public {
-        _lockAmount(address(0x43555), depositPeriod, lockAmount);
+    // Unit Test
+    function testFuzz__TimelockAsyncUnlock__LockAmount(address user, uint256 depositPeriod, uint256 lockAmount)
+        public
+    {
+        vm.assume(user != address(0));
+        _lockAmount(user, depositPeriod, lockAmount);
     }
 
-    function test__TimelockAsyncUnlock__RequestUnlock() public {
+    function test__TimelockAsyncUnlock__RequestUnlockInvalidArrayLength() public {
+        uint256[] memory depositPeriods = new uint256[](2);
+        depositPeriods[0] = 0;
+        depositPeriods[1] = 1;
+
+        uint256[] memory amounts = new uint256[](1);
+        amounts[0] = 1000;
+
+        // depositPeriods length and amounts length are different, expect revert
+        _requestUnlock(alice, alice, depositPeriods, amounts);
+    }
+
+    function test__TimelockAsyncUnlock__RequestUnlockAuthorizeCallerFailed() public {
         uint256[] memory depositPeriods = new uint256[](2);
         depositPeriods[0] = 0;
         depositPeriods[1] = 1;
 
         uint256[] memory amounts = new uint256[](2);
         amounts[0] = 1000;
-        amounts[1] = 2000;
-
-        for (uint256 i = 0; i < depositPeriods.length; i++) {
-            _lockAmount(address(0x43555), depositPeriods[i], amounts[i]);
-        }
-
-        amounts[0] = 500;
         amounts[1] = 1000;
 
-        _requestUnlock(address(0x43555), depositPeriods, amounts);
+        // Caller(alice) and owner(bob) are different, expect revert
+        _requestUnlock(alice, bob, depositPeriods, amounts);
     }
 
-    /**
-     * @dev deposit/lock
-     * This is the 1st action
-     */
     function _lockAmount(address user, uint256 depositPeriod, uint256 lockAmount) internal virtual {
         TimelockAsyncUnlock asyncUnlockInst = TimelockAsyncUnlock(asyncUnlock);
         uint256 prevLockedAmount = asyncUnlockInst.lockedAmount(user, depositPeriod);
@@ -64,7 +71,73 @@ contract TimelockAsyncUnlockTest is Test {
         assertEq(prevMaxRequestUnlock + lockAmount, asyncUnlockInst.maxRequestUnlock(user, depositPeriod));
     }
 
-    function _requestUnlock(address user, uint256[] memory _depositPeriods, uint256[] memory _amounts)
+    function _unlock(address caller, address user, uint256 requestId) internal virtual {
+        TimelockAsyncUnlock asyncUnlockInst = TimelockAsyncUnlock(asyncUnlock);
+        bool failed;
+
+        (uint256[] memory requestDepositPeriods, uint256[] memory requestAmounts) =
+            asyncUnlockInst.unlockRequests(user, requestId);
+
+        uint256[] memory prevUnlockRequestAmounts = new uint256[](requestDepositPeriods.length);
+
+        for (uint256 i = 0; i < requestDepositPeriods.length; ++i) {
+            prevUnlockRequestAmounts[i] =
+                asyncUnlockInst.unlockRequestAmountByDepositPeriod(user, requestDepositPeriods[i]);
+        }
+
+        if (caller != user) {
+            vm.expectRevert(
+                abi.encodeWithSelector(
+                    TimelockAsyncUnlock.TimelockAsyncUnlock__AuthorizeCallerFailed.selector, caller, user
+                )
+            );
+            failed = true;
+        } else if (requestId > asyncUnlockInst.currentPeriod()) {
+            vm.expectRevert(
+                abi.encodeWithSelector(
+                    TimelockAsyncUnlock.TimelockAsyncUnlock__UnlockBeforeUnlockPeriod.selector,
+                    caller,
+                    user,
+                    asyncUnlockInst.currentPeriod(),
+                    requestId
+                )
+            );
+            failed = true;
+        } else {
+            for (uint256 i = 0; i < requestDepositPeriods.length; ++i) {
+                if (requestId < requestDepositPeriods[i]) {
+                    vm.expectRevert(
+                        abi.encodeWithSelector(
+                            TimelockAsyncUnlock.TimelockAsyncUnlock__UnlockBeforeDepositPeriod.selector,
+                            caller,
+                            user,
+                            requestDepositPeriods[i],
+                            requestId
+                        )
+                    );
+                    failed = true;
+                    break;
+                }
+            }
+        }
+        vm.prank(caller);
+        asyncUnlockInst.unlock(user, requestId);
+
+        // in the case of success
+        if (!failed) {
+            /**
+             * Need to add MaxRequestUnlock; It's different based on redeem or unlock
+             */
+            for (uint256 i = 0; i < requestDepositPeriods.length; ++i) {
+                assertEq(
+                    prevUnlockRequestAmounts[i] - requestAmounts[i],
+                    asyncUnlockInst.unlockRequestAmountByDepositPeriod(user, requestDepositPeriods[i])
+                );
+            }
+        }
+    }
+
+    function _requestUnlock(address caller, address user, uint256[] memory _depositPeriods, uint256[] memory _amounts)
         internal
         virtual
     {
@@ -88,6 +161,13 @@ contract TimelockAsyncUnlockTest is Test {
                 )
             );
             failed = true;
+        } else if (caller != user) {
+            vm.expectRevert(
+                abi.encodeWithSelector(
+                    TimelockAsyncUnlock.TimelockAsyncUnlock__AuthorizeCallerFailed.selector, caller, user
+                )
+            );
+            failed = true;
         } else {
             for (uint256 i = 0; i < _depositPeriods.length; ++i) {
                 if (_amounts[i] > asyncUnlockInst.maxRequestUnlock(user, _depositPeriods[i])) {
@@ -107,8 +187,8 @@ contract TimelockAsyncUnlockTest is Test {
                 }
             }
         }
-        vm.prank(user);
-        asyncUnlockInst.requestUnlock(user, _depositPeriods, _amounts);
+        vm.prank(caller);
+        uint256 requestId = asyncUnlockInst.requestUnlock(user, _depositPeriods, _amounts);
 
         // in the case of success
         if (!failed) {
@@ -123,6 +203,24 @@ contract TimelockAsyncUnlockTest is Test {
                     asyncUnlockInst.maxRequestUnlock(user, _depositPeriods[i])
                 );
             }
+
+            //sume of araay
+            uint256 sumOfAmounts;
+            for (uint256 i = 0; i < _depositPeriods.length; ++i) {
+                sumOfAmounts += _amounts[i];
+            }
+
+            assertEq(sumOfAmounts, asyncUnlockInst.unlockRequestAmount(user, requestId));
+
+            (uint256[] memory requestDepositPeriods, uint256[] memory requestAmounts) =
+                asyncUnlockInst.unlockRequests(user, requestId);
+
+            assertEq(requestDepositPeriods, _depositPeriods);
+            assertEq(requestAmounts, _amounts);
+
+            //unlockRequestDepositPeriods
+            uint256[] memory requestDepositPeriodsOnly = asyncUnlockInst.unlockRequestDepositPeriods(user, requestId);
+            assertEq(requestDepositPeriodsOnly, _depositPeriods);
         }
     }
 
@@ -141,5 +239,11 @@ contract TimelockAsyncUnlockTest is Test {
         );
 
         return address(asyncUnlockInst);
+    }
+
+    // util functions
+    function _asSingletonArray(uint256 element) internal pure returns (uint256[] memory array) {
+        array = new uint256[](1);
+        array[0] = element;
     }
 }
