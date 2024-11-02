04455817ffc04b87391a9564c325582ed2c90567
Ian Lucas <ian.lucas@gmail.com>
2024-11-02 19:13:03 -0400

    feat: Skip deposits that have already been processed

--- packages/sdk/test/src/utils/logger.ts
+++ packages/sdk/test/src/utils/logger.ts
@@ -1,7 +1,35 @@
+import * as fs from 'fs';
 import * as path from 'path';
+import { PassThrough } from 'stream';
 import * as winston from 'winston';
 
 const logDirectory = path.join(__dirname, '../../../logs');
+export const processedLogCache: any[] = [];
+
+const processedLogFilePath = path.join(logDirectory, 'staking-processed.json');
+
+// Initialize logMessages from file, if it exists
+function initProcessedLogCache() {
+  console.log('Loading log messages into cache...');
+  if (!fs.existsSync(processedLogFilePath)) {
+    return;
+  }
+
+  const fileData = fs.readFileSync(processedLogFilePath, 'utf-8');
+  fileData
+    .trim()
+    .split('\n')
+    .forEach((line) => {
+      if (line) {
+        processedLogCache.push(JSON.parse(line));
+      }
+    });
+}
+
+const logStream = new PassThrough();
+logStream.on('data', (chunk) => {
+  processedLogCache.push(JSON.parse(chunk.toString())); // Parse and store each log entry
+});
 
 const logger = winston.createLogger({
   level: 'debug', // Set the default log level
@@ -23,8 +51,11 @@ const processedLogger = winston.createLogger({
     winston.format.json(), // Structured logging in JSON format
   ),
   transports: [
-    new winston.transports.File({ filename: path.join(logDirectory, 'staking-processed.json'), level: 'info' }),
+    new winston.transports.File({ filename: processedLogFilePath, level: 'info' }),
+    new winston.transports.Stream({ stream: logStream, level: 'info' }), // In-memory transport
   ],
 });
+
+initProcessedLogCache();
 
 export { logger, processedLogger };
