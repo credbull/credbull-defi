c8bcd807335bbb6de3c354b77869e31e6a126a08
Ian Lucas <ian.lucas@gmail.com>
2024-11-04 07:15:55 -0500

    chore: Add Load Test of 1000 deposits

--- packages/sdk/test/src/staking/vault-deposit-parser.spec.ts
+++ packages/sdk/test/src/staking/vault-deposit-parser.spec.ts
@@ -2,11 +2,13 @@ import { expect, test } from '@playwright/test';
 import { ethers } from 'ethers';
 
 import { VaultDeposit } from './vault-deposit';
-import { parseFromFile } from './vault-depost-parser';
+import { parseFromFile } from './vault-deposit-parser';
+
+const TEST_VAULT_DEPOSIT_3_FILENAME = 'TEST-vault-deposit-3.json';
 
 test.describe('Test Vault Deposit loader', () => {
   test('Test parse from file', async () => {
-    const vaultDeposits: VaultDeposit[] = parseFromFile('TEST-staking-data.json');
+    const vaultDeposits: VaultDeposit[] = parseFromFile(TEST_VAULT_DEPOSIT_3_FILENAME);
     expect(vaultDeposits.length).toBeGreaterThanOrEqual(1);
 
     for (const vaultDeposit of vaultDeposits) {
