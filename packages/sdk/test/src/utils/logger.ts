5ba4dbaf6f8741277f2897bb644b04d6a7630cac
Ian Lucas <ian.lucas@gmail.com>
2024-11-04 10:22:16 -0500

    chore: Reduce console log statements

--- packages/sdk/test/src/utils/logger.ts
+++ packages/sdk/test/src/utils/logger.ts
@@ -31,14 +31,12 @@ logStream.on('data', (chunk) => {
 });
 
 const logger = winston.createLogger({
-  level: 'debug', // Set the default log level
-
   format: winston.format.combine(
     winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
     winston.format.printf(({ timestamp, level, message }) => `${timestamp} [${level.toUpperCase()}]: ${message}`),
   ),
   transports: [
-    new winston.transports.Console(), // Log to console
+    new winston.transports.Console({ level: 'warn' }),
     new winston.transports.File({ filename: path.join(logDirectory, 'staking.log'), level: 'info' }),
   ],
 });
