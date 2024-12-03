4610518d55cbeac39c40cc9802c171a5c7fc71ec
Ian Lucas <ian.lucas@gmail.com>
2024-12-03 17:39:35 -0500

    chore: Remove unused test resources

--- packages/sdk/test/src/staking/vault-deposit.spec.ts
+++ packages/sdk/test/src/staking/vault-deposit.spec.ts
@@ -15,16 +15,6 @@ export const TEST_VAULT_DEPOSIT_50_FILENAME = 'TEST-vault-deposit-50.json';
 export const TEST_VAULT_DEPOSIT_1000_FILENAME = 'TEST-vault-deposit-1000.json';
 
 test.describe('Test Vault Deposit for all', () => {
-  test.skip('Load Test Deposit 1000', async () => {
-    test.setTimeout(3600000); // Set timeout to 1 hour (3,600,000 ms) - 1000 would take an hour on Arb!
-    const vaultDepositApp = new VaultDepositApp();
-
-    const result: LoadDepositResult = await vaultDepositApp.loadDeposits(TEST_VAULT_DEPOSIT_1000_FILENAME);
-    expect(result.successes.length).toBe(997); // IDs 7,8,9 will be skipped from Test Deposit 3
-    expect(result.fails.length).toBe(0);
-    expect(result.skipped.length).toBe(3); // IDs 7,8,9 will be skipped from Test Deposit 3
-  });
-
   test('Test Deposit empty json should process nothing', async () => {
     const vaultDepositApp = new VaultDepositApp();
 
