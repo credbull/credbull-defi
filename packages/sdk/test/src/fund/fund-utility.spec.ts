0d76a39bfb7e360f8deeaa0ed39ca57b028e98a3
Ian Lucas <ian.lucas@gmail.com>
2024-12-11 08:09:36 -0500

    chore: Add Fund Value Calculator output

--- packages/sdk/test/src/fund/fund-utility.spec.ts
+++ packages/sdk/test/src/fund/fund-utility.spec.ts
@@ -8,7 +8,8 @@ let config: Config;
 
 let userSigner: Wallet;
 
-const manualValueOracleAddress: string = '0x6748B0f4Cd17d1dE0CF718c747D6C0897B2922fb'; // Dec deploy
+const fundValueCalculatorAddress: string = '0xcdf038dd3b66506d2e5378aee185b2f0084b7a33';
+const manualValueOracleAddress: string = '0x6748B0f4Cd17d1dE0CF718c747D6C0897B2922fb'; // for flex loan 0xfa5d33621f94e596c8d18f3c4a2553ff0f6d538e
 
 test.beforeAll(async () => {
   config = loadConfiguration();
@@ -19,15 +20,27 @@ test.beforeAll(async () => {
 
 test.describe.skip('Test Fund', () => {
   test('Test Manual value oracle read functions', async () => {
-    const contract = new ethers.Contract(manualValueOracleAddress, ABI_MANUAL_VALUE_ORACLE, provider);
-    const connectedContract = contract.connect(userSigner);
+    const manualValueOracle = new ethers.Contract(manualValueOracleAddress, ABI_MANUAL_VALUE_ORACLE, provider);
+    const connectedContract = manualValueOracle.connect(userSigner);
     console.log('Updater: ', (await connectedContract.getUpdater()).toString());
     console.log('Value: ', (await connectedContract.getValue()).toString());
   });
 
+  test('Test Fund Value Calculator', async () => {
+    const fundValueCalculator = new ethers.Contract(fundValueCalculatorAddress, ABI_FUND_VALUE_CALCULATOR, provider);
+    const connectedContract = fundValueCalculator.connect(userSigner);
+
+    const vaultProxyAddress: string = '0x77b98d8105d0869194f745052896caa6f3dddaf7';
+
+    const [denominationAsset, navValue] = await connectedContract.callStatic.calcNav(vaultProxyAddress);
+
+    console.log('Denomination Asset:', denominationAsset);
+    console.log('NAV Value:', navValue.toString());
+  });
+
   test('Test Manual value oracle update value', async () => {
-    const contract = new ethers.Contract(manualValueOracleAddress, ABI_MANUAL_VALUE_ORACLE, provider);
-    const connectedContract = contract.connect(userSigner);
+    const manualValueOracle = new ethers.Contract(manualValueOracleAddress, ABI_MANUAL_VALUE_ORACLE, provider);
+    const connectedContract = manualValueOracle.connect(userSigner);
 
     const prevValue = (await connectedContract.getValue()).toNumber();
     console.log('Prev Value: ', prevValue);
@@ -71,6 +84,35 @@ test.describe.skip('Test Fund', () => {
   });
 });
 
+// see: https://polygonscan.com/address/0xcdf038dd3b66506d2e5378aee185b2f0084b7a33#code
+const ABI_FUND_VALUE_CALCULATOR = [
+  {
+    inputs: [
+      {
+        internalType: 'address',
+        name: '_vaultProxy',
+        type: 'address',
+      },
+    ],
+    name: 'calcNav',
+    outputs: [
+      {
+        internalType: 'address',
+        name: 'denominationAsset_',
+        type: 'address',
+      },
+      {
+        internalType: 'uint256',
+        name: 'nav_',
+        type: 'uint256',
+      },
+    ],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+];
+
+// see: https://github.com/enzymefinance/protocol/blob/dev/contracts/persistent/arbitrary-value-oracles/manual-value/ManualValueOracleLib.sol
 const ABI_MANUAL_VALUE_ORACLE = [
   {
     inputs: [],
